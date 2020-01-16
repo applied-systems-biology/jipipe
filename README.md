f7aeb0df7494c8c72b0f3efaefb0d802dfe511d0
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-01-16 13:14:02 +0100

    * Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Modular Automated Confrontation Assay Quantification Version 5 (ACAQ5)
+
+## Copyright
+
+Copyright by Zoltán Cseresnyés, Ruman Gerst
+
+Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo Figge
+
+https://www.leibniz-hki.de/en/applied-systems-biology.html
+
+HKI-Center for Systems Biology of Infection
+
+Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Insitute (HKI)
+
+Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+
+The project code is licensed under BSD 2-Clause.
+See the LICENSE file provided with the code for the full license.
+
+## Credits
+
+### Breeze icons
+
+Our plugin uses the [Breeze icon pack](https://github.com/KDE/breeze-icons) by KDE,
+licensed under [lesser GPL](https://raw.githubusercontent.com/KDE/breeze-icons/master/COPYING.LIB).
+
+### Font awesome icons
+
+Our plugin uses icons from [Font Awesome](https://fontawesome.com/) by Fonticons, Inc.
+We obtained the icons via the [Font-Awesome-SVG-PNG project](https://github.com/encharm/Font-Awesome-SVG-PNG) by Code Charm.
+The Font Awesome font is licensed under the [SIL OFL 1.1](http://scripts.sil.org/OFL).
+Font-Awesome-SVG-PNG is licensed under [MIT](https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/LICENSE).
---
+++ src/main/java/org/hkijena/acaq5/ACACCommand.java
@@ -0,0 +1,99 @@
+package org.hkijena.macaq;
+
+import io.scif.services.DatasetIOService;
+import net.imagej.DatasetService;
+import net.imagej.ImageJ;
+import net.imagej.ops.OpService;
+import org.scijava.app.StatusService;
+import org.scijava.command.Command;
+import org.scijava.command.CommandService;
+import org.scijava.display.DisplayService;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.PluginService;
+import org.scijava.thread.ThreadService;
+import org.scijava.ui.UIService;
+
+import javax.swing.*;
+
+@Plugin(type = Command.class, menuPath = "Plugins>mACAQ")
+public class ACACCommand implements Command {
+    @Parameter
+    private OpService ops;
+
+    @Parameter
+    private LogService log;
+
+    @Parameter
+    private UIService ui;
+
+    @Parameter
+    private CommandService cmd;
+
+    @Parameter
+    private StatusService status;
+
+    @Parameter
+    private ThreadService thread;
+
+    @Parameter
+    private DatasetIOService datasetIO;
+
+    @Parameter
+    private DisplayService display;
+
+    @Parameter
+    private DatasetService datasetService;
+
+    @Parameter
+    private PluginService pluginService;
+
+    @Override
+    public void run() {
+//        MISAImageJRegistryService.instantiate(pluginService);
+//        SwingUtilities.invokeLater(() -> {
+//            ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
+//            ToolTipManager.sharedInstance().setInitialDelay(1000);
+//            MISAModuleRepositoryUI.getInstance(this).setVisible(true);
+//        });
+    }
+
+    public LogService getLogService() {
+        return log;
+    }
+
+    public StatusService getStatusService() {
+        return status;
+    }
+
+    public ThreadService getThreadService() {
+        return thread;
+    }
+
+    public UIService getUiService() {
+        return ui;
+    }
+
+    public DatasetIOService getDatasetIOService() {
+        return datasetIO;
+    }
+
+    public DisplayService getDisplayService() {
+        return display;
+    }
+
+    public DatasetService getDatasetService() {
+        return datasetService;
+    }
+
+    public PluginService getPluginService() {
+        return pluginService;
+    }
+
+    public static void main(final String... args) {
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+        ij.command().run(ACACCommand.class, true);
+    }
+}
