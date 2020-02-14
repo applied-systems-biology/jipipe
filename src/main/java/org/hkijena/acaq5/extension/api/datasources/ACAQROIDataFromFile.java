6ce722b7c2fb1e21afdd71455120e2dce876b649
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-02-14 15:57:44 +0100

    * ROI saving/loading equal to ImageJ

--- src/main/java/org/hkijena/acaq5/extension/api/datasources/ACAQROIDataFromFile.java
+++ src/main/java/org/hkijena/acaq5/extension/api/datasources/ACAQROIDataFromFile.java
@@ -1,12 +1,19 @@
 package org.hkijena.acaq5.extension.api.datasources;
 
+import ij.gui.Roi;
 import ij.io.RoiDecoder;
 import org.hkijena.acaq5.api.*;
 import org.hkijena.acaq5.extension.api.dataslots.ACAQROIDataSlot;
 import org.hkijena.acaq5.extension.api.datatypes.ACAQROIData;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Loads ROI data from a file via IJ.openFile()
@@ -30,12 +37,52 @@ public class ACAQROIDataFromFile extends ACAQSimpleDataSource<ACAQROIData> {
 
     @Override
     public void run() {
-        RoiDecoder decoder = new RoiDecoder(fileName.toString());
+
+        // Code adapted from ImageJ RoiManager
+        List<Roi> result = new ArrayList<>();
+        ZipInputStream in = null;
+        ByteArrayOutputStream out = null;
+        int nRois = 0;
         try {
-            setOutputData(new ACAQROIData(decoder.getRoi()));
+            in = new ZipInputStream(new FileInputStream(fileName.toFile()));
+            byte[] buf = new byte[1024];
+            int len;
+            ZipEntry entry = in.getNextEntry();
+            while (entry != null) {
+                String name = entry.getName();
+                if (name.endsWith(".roi")) {
+                    out = new ByteArrayOutputStream();
+                    while ((len = in.read(buf)) > 0)
+                        out.write(buf, 0, len);
+                    out.close();
+                    byte[] bytes = out.toByteArray();
+                    RoiDecoder rd = new RoiDecoder(bytes, name);
+                    Roi roi = rd.getRoi();
+                    if (roi != null) {
+                        name = name.substring(0, name.length() - 4);
+                        result.add(roi);
+                        nRois++;
+                    }
+                }
+                entry = in.getNextEntry();
+            }
+            in.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            if (in != null)
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            if (out != null)
+                try {
+                    out.close();
+                } catch (IOException e) {
+                }
         }
+
+        setOutputData(new ACAQROIData(result));
     }
 
     @ACAQParameter("file-name")
--- src/main/java/org/hkijena/acaq5/extension/api/datatypes/ACAQROIData.java
+++ src/main/java/org/hkijena/acaq5/extension/api/datatypes/ACAQROIData.java
@@ -1,26 +1,50 @@
 package org.hkijena.acaq5.extension.api.datatypes;
 
+import ij.IJ;
 import ij.gui.Roi;
+import ij.io.RoiEncoder;
 import ij.plugin.frame.RoiManager;
 import org.hkijena.acaq5.api.ACAQData;
 import org.hkijena.acaq5.api.ACAQDocumentation;
 
+import java.io.*;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @ACAQDocumentation(name = "ROI", description = "Collection of ROI")
 public class ACAQROIData implements ACAQData {
-    private Roi roi;
+    private List<Roi> roi;
 
-    public ACAQROIData(Roi roi) {
+    public ACAQROIData(List<Roi> roi) {
         this.roi = roi;
     }
 
-    public Roi getROI() {
+    public List<Roi> getROI() {
         return roi;
     }
 
     @Override
     public void saveTo(Path storageFilePath, String name) {
-        throw new RuntimeException("Not implemented yet");
+        // Code adapted from ImageJ RoiManager class
+        try {
+            ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(storageFilePath.resolve(name + ".roi").toFile())));
+            DataOutputStream out = new DataOutputStream(new BufferedOutputStream(zos));
+            RoiEncoder re = new RoiEncoder(out);
+            for (int i = 0; i < this.roi.size(); i++) {
+                String label = name + "-" + i;
+                Roi roi = this.roi.get(i);
+                if (roi == null) continue;
+                if (!label.endsWith(".roi")) label += ".roi";
+                zos.putNextEntry(new ZipEntry(label));
+                re.write(roi);
+                out.flush();
+            }
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
