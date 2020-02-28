e5648cb44a7ba8debecf4fb3496b221bd4c6679e
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-02-28 11:37:44 +0100

    + Saving operations for filesystem data

--- src/main/java/org/hkijena/acaq5/api/filesystem/dataypes/ACAQFileData.java
+++ src/main/java/org/hkijena/acaq5/api/filesystem/dataypes/ACAQFileData.java
@@ -1,8 +1,12 @@
 package org.hkijena.acaq5.api.filesystem.dataypes;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import org.hkijena.acaq5.api.ACAQDocumentation;
 import org.hkijena.acaq5.api.data.ACAQData;
+import org.hkijena.acaq5.utils.JsonUtils;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -20,9 +24,26 @@ public class ACAQFileData implements ACAQData {
 
     @Override
     public void saveTo(Path storageFilePath, String name) {
+        Path jsonFile = storageFilePath.resolve(name + ".json");
+        try {
+            JsonUtils.getObjectMapper().writerWithDefaultPrettyPrinter().writeValue(jsonFile.toFile(), this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @JsonGetter("acaq:data-class")
+    private String getDataTypeName() {
+        return getClass().getCanonicalName();
+    }
+
+    @JsonSetter("file-path")
     public Path getFilePath() {
         return filePath;
+    }
+
+    @JsonSetter("file-path")
+    private void setFilePath(Path path) {
+        this.filePath = path;
     }
 }
--- src/main/java/org/hkijena/acaq5/api/filesystem/dataypes/ACAQFolderData.java
+++ src/main/java/org/hkijena/acaq5/api/filesystem/dataypes/ACAQFolderData.java
@@ -1,8 +1,12 @@
 package org.hkijena.acaq5.api.filesystem.dataypes;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import org.hkijena.acaq5.api.ACAQDocumentation;
 import org.hkijena.acaq5.api.data.ACAQData;
+import org.hkijena.acaq5.utils.JsonUtils;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -20,9 +24,26 @@ public class ACAQFolderData implements ACAQData {
 
     @Override
     public void saveTo(Path storageFilePath, String name) {
+        Path jsonFile = storageFilePath.resolve(name + ".json");
+        try {
+            JsonUtils.getObjectMapper().writerWithDefaultPrettyPrinter().writeValue(jsonFile.toFile(), this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @JsonGetter("acaq:data-class")
+    private String getDataTypeName() {
+        return getClass().getCanonicalName();
+    }
+
+    @JsonGetter("folder-path")
     public Path getFolderPath() {
         return folderPath;
+    }
+
+    @JsonSetter("folder-path")
+    private void setFolderPath(Path path) {
+        this.folderPath = path;
     }
 }
