5c443c0bf29099bb4dcd0e128dee152f23a3517e
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-05-12 11:54:19 +0200

    + Improved numeric parameter editor

--- src/main/java/org/hkijena/acaq5/extensions/parameters/editors/NumberParameterEditorUI.java
+++ src/main/java/org/hkijena/acaq5/extensions/parameters/editors/NumberParameterEditorUI.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * Editor for a {@link Double} parameter
+ * Editor for a any numeric parameter
  */
 public class NumberParameterEditorUI extends ACAQParameterEditorUI {
     private JSpinner spinner;
@@ -61,6 +61,11 @@ public class NumberParameterEditorUI extends ACAQParameterEditorUI {
     }
 
     private Comparable<?> getMinimumValue() {
+        NumberParameterSettings settings = getParameterAccess().getAnnotationOfType(NumberParameterSettings.class);
+        if(settings != null) {
+            return settings.min();
+        }
+
         if (getParameterAccess().getFieldClass() == byte.class || getParameterAccess().getFieldClass() == Byte.class) {
             return Byte.MIN_VALUE;
         } else if (getParameterAccess().getFieldClass() == short.class || getParameterAccess().getFieldClass() == Short.class) {
@@ -79,6 +84,11 @@ public class NumberParameterEditorUI extends ACAQParameterEditorUI {
     }
 
     private Comparable<?> getMaximumValue() {
+        NumberParameterSettings settings = getParameterAccess().getAnnotationOfType(NumberParameterSettings.class);
+        if(settings != null) {
+            return settings.max();
+        }
+
         if (getParameterAccess().getFieldClass() == byte.class || getParameterAccess().getFieldClass() == Byte.class) {
             return Byte.MAX_VALUE;
         } else if (getParameterAccess().getFieldClass() == short.class || getParameterAccess().getFieldClass() == Short.class) {
@@ -94,6 +104,14 @@ public class NumberParameterEditorUI extends ACAQParameterEditorUI {
         } else {
             throw new IllegalArgumentException("Unsupported numeric type: " + getParameterAccess().getFieldClass());
         }
+    }
+
+    private double getStep() {
+        NumberParameterSettings settings = getParameterAccess().getAnnotationOfType(NumberParameterSettings.class);
+        if(settings != null) {
+            return settings.step();
+        }
+        return 1;
     }
 
     private boolean setCurrentValue(Number number) {
@@ -116,7 +134,7 @@ public class NumberParameterEditorUI extends ACAQParameterEditorUI {
 
     private void initialize() {
         setLayout(new BorderLayout());
-        SpinnerNumberModel model = new SpinnerNumberModel(getCurrentValue(), getMinimumValue(), getMaximumValue(), 1);
+        SpinnerNumberModel model = new SpinnerNumberModel(getCurrentValue(), getMinimumValue(), getMaximumValue(), getStep());
         spinner = new JSpinner(model);
         spinner.addChangeListener(e -> {
             if (!isReloading) {
---
+++ src/main/java/org/hkijena/acaq5/extensions/parameters/editors/NumberParameterSettings.java
@@ -0,0 +1,31 @@
+package org.hkijena.acaq5.extensions.parameters.editors;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Attached to the getter or setter of {@link Number} parameter to setup the GUI
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NumberParameterSettings {
+    /**
+     * Minimum value
+     * @return Minimum value
+     */
+    double min() default Double.NEGATIVE_INFINITY;
+
+    /**
+     * Maximum value
+     * @return Maximum value
+     */
+    double max() default Double.POSITIVE_INFINITY;
+
+    /**
+     * The step size when the user clicks increase/decrease
+     * @return step size when the user clicks increase/decrease
+     */
+    double step() default 1;
+}
