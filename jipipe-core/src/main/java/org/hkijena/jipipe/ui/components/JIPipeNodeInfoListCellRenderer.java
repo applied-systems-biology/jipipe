1ae6292c9551ca2bce3345712672282bdbf28016
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-07-19 22:50:00 +0200

    * Improved algorithm compendium performance

--- jipipe-core/src/main/java/org/hkijena/jipipe/ui/components/JIPipeNodeInfoListCellRenderer.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/ui/components/JIPipeNodeInfoListCellRenderer.java
@@ -25,18 +25,60 @@ import java.awt.*;
 /**
  * Renderer for {@link JIPipeNodeInfo}
  */
-public class JIPipeNodeInfoListCellRenderer extends JLabel implements ListCellRenderer<JIPipeNodeInfo> {
+public class JIPipeNodeInfoListCellRenderer extends JPanel implements ListCellRenderer<JIPipeNodeInfo> {
 
-    private ColorIcon icon = new ColorIcon(16, 40);
+    private ColorIcon nodeColor;
+    private JLabel nodeIcon;
+    private JLabel nameLabel;
+    private JLabel pathLabel;
 
     /**
      * Creates a new renderer
      */
     public JIPipeNodeInfoListCellRenderer() {
         setOpaque(true);
-        setIcon(icon);
         setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
-        setVerticalAlignment(TOP);
+        initialize();
+    }
+
+    private void initialize() {
+        setLayout(new GridBagLayout());
+        nodeColor = new ColorIcon(16, 40);
+        nodeIcon = new JLabel();
+        nameLabel = new JLabel();
+        pathLabel = new JLabel();
+        pathLabel.setForeground(Color.GRAY);
+
+        add(new JLabel(nodeColor), new GridBagConstraints() {
+            {
+                gridx = 0;
+                gridy = 0;
+                gridheight = 2;
+            }
+        });
+        add(nodeIcon, new GridBagConstraints() {
+            {
+                gridx = 1;
+                gridy = 0;
+                insets = new Insets(0,4,0, 4);
+            }
+        });
+        add(nameLabel, new GridBagConstraints() {
+            {
+                gridx = 2;
+                gridy = 0;
+                fill = HORIZONTAL;
+                weightx = 1;
+            }
+        });
+        add(pathLabel, new GridBagConstraints() {
+            {
+                gridx = 2;
+                gridy = 1;
+                fill = HORIZONTAL;
+                weightx = 1;
+            }
+        });
     }
 
     @Override
@@ -45,22 +87,16 @@ public class JIPipeNodeInfoListCellRenderer extends JLabel implements ListCellRe
         setFont(list.getFont());
 
         if (info != null) {
-            icon.setFillColor(UIUtils.getFillColorFor(info));
+            nodeColor.setFillColor(UIUtils.getFillColorFor(info));
             String menuPath = info.getCategory().toString();
             menuPath += "\n" + info.getMenuPath();
             menuPath = StringUtils.getCleanedMenuPath(menuPath).replace("\n", " > ");
-            setText(String.format("<html><table cellpadding=\"1\"><tr>" +
-                            "<td><img src=\"%s\"/></td>" +
-                            "<td>%s</td></tr>" +
-                            "<tr>" +
-                            "<td></td>" +
-                            "<td><span style=\"color: gray;\">%s</span></td></tr></table></html>",
-                    JIPipeUIAlgorithmRegistry.getInstance().getIconURLFor(info),
-                    HtmlEscapers.htmlEscaper().escape(info.getName()),
-                    menuPath
-            ));
+
+            pathLabel.setText(menuPath);
+            nameLabel.setText(info.getName());
+            nodeIcon.setIcon(JIPipeUIAlgorithmRegistry.getInstance().getIconFor(info));
         } else {
-            setText("<Null>");
+            nameLabel.setText("<Null>");
         }
 
         if (isSelected) {
