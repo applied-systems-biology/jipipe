df0b9adde81c6a2f0dd0efb1283b0cbeef86b830
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-08-12 15:15:20 +0200

    * updated SciJava

--- jipipe-ij/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
+++ jipipe-ij/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
@@ -163,12 +163,19 @@ public class ImageJAlgorithmsExtension extends JIPipePrepackagedDefaultJavaExten
                 "and Jason R. Swedlow (2010) Metadata matters: access to image data in the real world. The Journal of Cell Biology 189(5), 777-782");
         result.add("Legland, D.; Arganda-Carreras, I. & Andrey, P. (2016), \"MorphoLibJ: integrated library and plugins for mathematical morphology with ImageJ\", " +
                 "Bioinformatics (Oxford Univ Press) 32(22): 3532-3534, PMID 27412086, doi:10.1093/bioinformatics/btw413");
+        result.add("Eliceiri K. V., Berthold M. R., Goldberg I. G., Ibanez L., Manjunath B. S., Martone M. E., Murphy R. F., Peng H., Plant A. L., Roysam B., Stuurmann N.," +
+                " Swedlow J.R., Tomancak P., Carpenter A. E. (2012) Biological Imaging Software Tools Nature Methods 9(7), 697-710");
+        result.add("FeatureJ by Erik Meijering. http://imagescience.org/meijering/software/featurej/");
+        result.add("MTrackJ by Erik Meijering. https://imagescience.org/meijering/software/mtrackj/");
+        result.add("RandomJ by Erik Meijering. https://imagescience.org/meijering/software/randomj/");
+        result.add("ImageScience by Erik Meijering. https://imagescience.org/meijering/software/imagescience/");
         return result;
     }
 
     @Override
     public List<JIPipeImageJUpdateSiteDependency> getImageJUpdateSiteDependencies() {
-        return Arrays.asList(new JIPipeImageJUpdateSiteDependency("IJPB-plugins", "https://sites.imagej.net/IJPB-plugins/"));
+        return Arrays.asList(new JIPipeImageJUpdateSiteDependency("IJPB-plugins", "https://sites.imagej.net/IJPB-plugins/"),
+                new JIPipeImageJUpdateSiteDependency("ImageScience", "https://sites.imagej.net/ImageScience/"));
     }
 
     @Override
