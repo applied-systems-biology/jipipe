92b67cc77247e9337c81d0087150c58f3572090d
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-10-14 00:03:42 +0200

    * updated

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,21 @@ $(document).ready(function(){
   $('.landing-features').slick({
     infinite: true,
     dots: true,
-    slidesToShow: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 10000,
     centerMode: true,
-    centerPadding: '5rem'
+    centerPadding: '5rem',
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
