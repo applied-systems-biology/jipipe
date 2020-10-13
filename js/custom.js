a90bdd4ff97e52b14303716761c872f5afdfca91
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-10-13 14:09:44 +0200

    + New landing page

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.landing-features').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    centerMode: true,
+    centerPadding: '60px'
+  });
+});
