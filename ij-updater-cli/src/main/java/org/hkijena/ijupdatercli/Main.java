ab7085ae3c8212471d974af74f8a9c6935b19a45
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2020-10-15 21:51:14 +0200

    + Working updater

--- ij-updater-cli/src/main/java/org/hkijena/ijupdatercli/Main.java
+++ ij-updater-cli/src/main/java/org/hkijena/ijupdatercli/Main.java
@@ -1,10 +1,12 @@
 package org.hkijena.ijupdatercli;
 
+import net.imagej.ui.swing.updater.ProgressDialog;
 import net.imagej.ui.swing.updater.SwingAuthenticator;
 import net.imagej.updater.FilesCollection;
 import net.imagej.updater.Installer;
 import net.imagej.updater.UpdateSite;
 import net.imagej.updater.util.AvailableSites;
+import net.imagej.updater.util.Progress;
 import net.imagej.updater.util.StderrProgress;
 import net.imagej.updater.util.UpdaterUtil;
 import org.scijava.util.AppUtils;
@@ -15,25 +17,25 @@ import java.io.File;
 import java.io.IOException;
 import java.net.Authenticator;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length == 0 || args[0].contains("help")) {
-            System.out.println("IJ Updater CLI");
-            System.out.println("--------------");
-            System.out.println("Part of JIPipe https://www.jipipe.org/");
-            System.out.println("Developed by Applied Systems Biology, HKI Jena, Germany");
-            System.out.println();
-            System.out.println("activate <Update Site Name> <Update Site Name> ...");
-            System.out.println("deactivate <Update Site Name> <Update Site Name> ...");
-            System.out.println("add <Update Site Name> <URL>");
-            System.out.println("remove <Update Site Name>");
-            System.out.println();
-            System.out.println("To run this tool, execute following command:");
-            System.out.println("<ImageJ executable> --pass-classpath --full-classpath --main-class org.hkijena.ijupdatercli.Main");
+        if(args.length == 0) {
+            showHelp();
+            return;
+        }
+
+        int firstArgIndex = 0;
+        while(firstArgIndex < args.length && args[firstArgIndex].startsWith("-")) {
+            firstArgIndex += 2;
+        }
+
+        if(args[firstArgIndex].contains("help")) {
+           showHelp();
             return;
         }
 
@@ -46,17 +48,14 @@ public class Main {
 
             filesCollection = new FilesCollection(getImageJRoot().toFile());
             AvailableSites.initializeAndAddSites(filesCollection);
-            filesCollection.downloadIndexAndChecksum(new StderrProgress());
+            filesCollection.downloadIndexAndChecksum(createProgress());
         } catch (Exception e) {
             e.printStackTrace();
             return;
         }
 
-        if(Objects.equals(args[0], "activate")) {
-            Set<String> toActivateNames = new HashSet<>();
-            for (int i = 1; i < args.length; i++) {
-                toActivateNames.add(args[i]);
-            }
+        if(Objects.equals(args[firstArgIndex], "activate")) {
+            Set<String> toActivateNames = new HashSet<>(Arrays.asList(args).subList(firstArgIndex + 1, args.length));
             Set<UpdateSite> toActivate = new HashSet<>();
             for (UpdateSite updateSite : filesCollection.getUpdateSites(true)) {
                 if(toActivateNames.contains(updateSite.getName())) {
@@ -75,7 +74,7 @@ public class Main {
             for (UpdateSite updateSite : toActivate) {
                 if(!updateSite.isActive()) {
                     try {
-                        filesCollection.activateUpdateSite(updateSite, new StderrProgress());
+                        filesCollection.activateUpdateSite(updateSite, createProgress());
                     } catch (ParserConfigurationException | IOException | SAXException e) {
                         throw new RuntimeException(e);
                     }
@@ -83,11 +82,8 @@ public class Main {
             }
             applyUpdates(filesCollection);
         }
-        else if(Objects.equals(args[0], "deactivate")) {
-            Set<String> toDeactivateNames = new HashSet<>();
-            for (int i = 1; i < args.length; i++) {
-                toDeactivateNames.add(args[i]);
-            }
+        else if(Objects.equals(args[firstArgIndex], "deactivate")) {
+            Set<String> toDeactivateNames = new HashSet<>(Arrays.asList(args).subList(firstArgIndex + 1, args.length));
             Set<UpdateSite> toDeactivate = new HashSet<>();
             for (UpdateSite updateSite : filesCollection.getUpdateSites(true)) {
                 if(toDeactivateNames.contains(updateSite.getName())) {
@@ -102,11 +98,11 @@ public class Main {
             }
             applyUpdates(filesCollection);
         }
-        else if(Objects.equals(args[0], "add")) {
-            String name = args[1];
-            String url = args[2];
+        else if(Objects.equals(args[firstArgIndex], "add")) {
+            String name = args[firstArgIndex + 1];
+            String url = args[firstArgIndex + 2];
             for (UpdateSite updateSite : filesCollection.getUpdateSites(true)) {
-                if(Objects.equals(name, updateSite)) {
+                if(Objects.equals(name, updateSite.getName())) {
                     System.out.println("Update site already exists. Nothing to do.");
                     return;
                 }
@@ -115,16 +111,61 @@ public class Main {
             filesCollection.addUpdateSite(site);
             applyUpdates(filesCollection);
         }
-        else if(Objects.equals(args[0], "remove")) {
+        else if(Objects.equals(args[firstArgIndex], "remove")) {
             String name = args[1];
             filesCollection.removeUpdateSite(name);
             applyUpdates(filesCollection);
         }
+        else if(Objects.equals(args[firstArgIndex], "list")) {
+            boolean withActive = firstArgIndex == args.length - 1 || args[firstArgIndex + 1].equals("active");
+            boolean withInactive = firstArgIndex == args.length - 1 || args[firstArgIndex + 1].equals("inactive");
+            for (UpdateSite updateSite : filesCollection.getUpdateSites(true)) {
+                if(updateSite.isActive() && withActive || !updateSite.isActive() && withInactive) {
+                    System.out.println(updateSite.getName() + "\t" + updateSite.getURL() + "\t" + (updateSite.isActive() ? "active" : "inactive"));
+                }
+            }
+        }
+        else if(Objects.equals(args[firstArgIndex], "update")) {
+            applyUpdates(filesCollection);
+        }
+        else {
+            throw new RuntimeException("Invalid command!");
+        }
+    }
+
+    public static Progress createProgress() {
+        if(getOperatingSystem().toLowerCase().contains("windows")) {
+            return new ProgressDialog(null, "ImageJ updater");
+        }
+        else {
+            return new StderrProgress();
+        }
+    }
+
+    public static String getOperatingSystem() {
+        return System.getProperty("os.name");
+    }
+
+    private static void showHelp() {
+        System.out.println("IJ Updater CLI");
+        System.out.println("--------------");
+        System.out.println("Part of JIPipe https://www.jipipe.org/");
+        System.out.println("Developed by Applied Systems Biology, HKI Jena, Germany");
+        System.out.println();
+        System.out.println("list [active|inactive]");
+        System.out.println("update");
+        System.out.println("activate <Update Site Name> <Update Site Name> ...");
+        System.out.println("deactivate <Update Site Name> <Update Site Name> ...");
+        System.out.println("add <Update Site Name> <URL>");
+        System.out.println("remove <Update Site Name>");
+        System.out.println();
+        System.out.println("To run this tool, execute following command:");
+        System.out.println("<ImageJ executable> --debug --pass-classpath --full-classpath --main-class org.hkijena.ijupdatercli.Main");
     }
 
     public static void applyUpdates(FilesCollection filesCollection) {
         final Installer installer =
-                new Installer(filesCollection, new StderrProgress());
+                new Installer(filesCollection, createProgress());
         try {
             installer.start();
             filesCollection.write();
