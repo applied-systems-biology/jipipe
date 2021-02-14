ce3ca8c2e59dd059c3fcb59c2a1bce7218178a90
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-02-14 15:25:59 +0100

    * fix: Skeletonize is inverted

--- jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ij1/morphology/MorphologySkeletonize2DAlgorithm.java
+++ jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ij1/morphology/MorphologySkeletonize2DAlgorithm.java
@@ -21,6 +21,7 @@ import org.hkijena.jipipe.api.JIPipeProgressInfo;
 import org.hkijena.jipipe.api.data.JIPipeDefaultMutableSlotConfiguration;
 import org.hkijena.jipipe.api.nodes.*;
 import org.hkijena.jipipe.api.nodes.categories.ImagesNodeTypeCategory;
+import org.hkijena.jipipe.api.parameters.JIPipeParameter;
 import org.hkijena.jipipe.extensions.imagejdatatypes.datatypes.ImagePlusData;
 import org.hkijena.jipipe.extensions.imagejdatatypes.datatypes.greyscale.ImagePlusGreyscaleMaskData;
 import org.hkijena.jipipe.extensions.imagejdatatypes.util.ImageJUtils;
@@ -34,6 +35,8 @@ import org.hkijena.jipipe.extensions.imagejdatatypes.util.ImageJUtils;
 @JIPipeInputSlot(value = ImagePlusGreyscaleMaskData.class, slotName = "Input")
 @JIPipeOutputSlot(value = ImagePlusGreyscaleMaskData.class, slotName = "Output")
 public class MorphologySkeletonize2DAlgorithm extends JIPipeSimpleIteratingAlgorithm {
+
+    private boolean darkBackground = true;
 
     /**
      * Instantiates a new node type.
@@ -55,6 +58,7 @@ public class MorphologySkeletonize2DAlgorithm extends JIPipeSimpleIteratingAlgor
      */
     public MorphologySkeletonize2DAlgorithm(MorphologySkeletonize2DAlgorithm other) {
         super(other);
+        this.darkBackground = other.darkBackground;
     }
 
     @Override
@@ -66,7 +70,18 @@ public class MorphologySkeletonize2DAlgorithm extends JIPipeSimpleIteratingAlgor
     protected void runIteration(JIPipeDataBatch dataBatch, JIPipeProgressInfo progressInfo) {
         ImagePlusData inputData = dataBatch.getInputData(getFirstInputSlot(), ImagePlusGreyscaleMaskData.class, progressInfo);
         ImagePlus img = inputData.getDuplicateImage();
-        ImageJUtils.forEachSlice(img, ip -> ((ByteProcessor) ip).skeletonize(), progressInfo);
+        ImageJUtils.forEachSlice(img, ip -> ((ByteProcessor) ip).skeletonize(darkBackground ? 255 : 0), progressInfo);
         dataBatch.addOutputData(getFirstOutputSlot(), new ImagePlusGreyscaleMaskData(img), progressInfo);
+    }
+
+    @JIPipeDocumentation(name = "Dark background", description = "If enabled, pixel values of 255 are considered as foreground.")
+    @JIPipeParameter("dark-background")
+    public boolean isDarkBackground() {
+        return darkBackground;
+    }
+
+    @JIPipeParameter("dark-background")
+    public void setDarkBackground(boolean darkBackground) {
+        this.darkBackground = darkBackground;
     }
 }
