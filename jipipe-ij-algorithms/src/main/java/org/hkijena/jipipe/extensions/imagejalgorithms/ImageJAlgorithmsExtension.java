cd4908d65cc24d3a628c40116c018be40d29ed6b
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-03-01 20:38:05 +0100

    + Reorder ROI dimensions

--- jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
+++ jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
@@ -648,6 +648,7 @@ public class ImageJAlgorithmsExtension extends JIPipePrepackagedDefaultJavaExten
         registerNodeType("ij1-roi-filter-by-overlap", FilterROIByOverlapAlgorithm.class, UIUtils.getIconURLFromResources("actions/filter.png"));
         registerNodeType("ij1-roi-generate-name", GenerateROINameAlgorithm.class, UIUtils.getIconURLFromResources("actions/tag.png"));
         registerNodeType("ij1-roi-filter-roi-lists", FilterROIListsAlgorithm.class, UIUtils.getIconURLFromResources("actions/filter.png"));
+        registerNodeType("ij1-roi-dimension-reorder", ReorderRoiDimensionsAlgorithm.class, UIUtils.getIconURLFromResources("actions/split.png"));
 
         registerEnumParameterType("ij1-roi-from-table-rectangular:anchor",
                 TableToRectangularROIAlgorithm.Anchor.class,
