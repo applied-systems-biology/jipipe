151725954b3058215ba470b0cc6e0614efb7554c
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-06-04 10:51:09 +0200

    + h5py fix

--- jipipe-deep-learning/src/main/java/org/hkijena/jipipe/extensions/deeplearning/DeepLearningToolkitEnvInstaller.java
+++ jipipe-deep-learning/src/main/java/org/hkijena/jipipe/extensions/deeplearning/DeepLearningToolkitEnvInstaller.java
@@ -3,6 +3,7 @@ package org.hkijena.jipipe.extensions.deeplearning;
 import org.hkijena.jipipe.api.JIPipeDocumentation;
 import org.hkijena.jipipe.api.parameters.JIPipeParameter;
 import org.hkijena.jipipe.api.parameters.JIPipeParameterAccess;
+import org.hkijena.jipipe.extensions.parameters.primitives.OptionalStringParameter;
 import org.hkijena.jipipe.extensions.python.installers.BasicMinicondaEnvPythonInstaller;
 import org.hkijena.jipipe.extensions.settings.RuntimeSettings;
 import org.hkijena.jipipe.ui.JIPipeWorkbench;
@@ -58,13 +59,21 @@ public class DeepLearningToolkitEnvInstaller extends BasicMinicondaEnvPythonInst
                     "opencv",
                     "tqdm",
                     "tifffile",
-                    "h5py",
                     "keras>=2.3.1")) {
                 writer.write("  - ");
                 writer.write(dependency);
                 writer.newLine();
             }
             Configuration configuration = (Configuration) getConfiguration();
+            if(configuration.getH5pyVersion().isEnabled()) {
+                writer.write("  - h5py==");
+                writer.write(configuration.getH5pyVersion().getContent());
+                writer.newLine();
+            }
+            else {
+                writer.write("  - h5py");
+                writer.newLine();
+            }
             if (configuration.isWithGPU()) {
                 writer.write("  - tensorflow-gpu==");
                 writer.write(configuration.getTensorFlowVersion());
@@ -86,6 +95,7 @@ public class DeepLearningToolkitEnvInstaller extends BasicMinicondaEnvPythonInst
     public static class Configuration extends BasicMinicondaEnvPythonInstaller.Configuration {
         private boolean withGPU = true;
         private String tensorFlowVersion = "2.1.0";
+        private OptionalStringParameter h5pyVersion = new OptionalStringParameter("2.10.0", true);
 
         @JIPipeDocumentation(name = "GPU support", description = "If enabled, install Tensorflow with GPU support.")
         @JIPipeParameter("with-gpu")
@@ -109,6 +119,17 @@ public class DeepLearningToolkitEnvInstaller extends BasicMinicondaEnvPythonInst
         @JIPipeParameter("tensorflow-version")
         public void setTensorFlowVersion(String tensorFlowVersion) {
             this.tensorFlowVersion = tensorFlowVersion;
+        }
+
+        @JIPipeDocumentation(name = "h5py version", description = "If enabled, sets the version of h5py. There is a known bug with h5py versions >= 3.0 https://github.com/tensorflow/tensorflow/issues/44467")
+        @JIPipeParameter("h5py-version")
+        public OptionalStringParameter getH5pyVersion() {
+            return h5pyVersion;
+        }
+
+        @JIPipeParameter("h5py-version")
+        public void setH5pyVersion(OptionalStringParameter h5pyVersion) {
+            this.h5pyVersion = h5pyVersion;
         }
     }
 }
