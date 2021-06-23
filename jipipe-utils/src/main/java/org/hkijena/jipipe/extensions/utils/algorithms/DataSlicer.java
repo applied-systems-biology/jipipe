b966b0a0dc7f85c016c6174898de12c44c69c672
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-06-23 15:50:27 +0200

    + Data slicer node

--- jipipe-utils/src/main/java/org/hkijena/jipipe/extensions/utils/algorithms/DataSlicer.java
+++ jipipe-utils/src/main/java/org/hkijena/jipipe/extensions/utils/algorithms/DataSlicer.java
@@ -3,44 +3,47 @@ package org.hkijena.jipipe.extensions.utils.algorithms;
 import org.hkijena.jipipe.api.JIPipeDocumentation;
 import org.hkijena.jipipe.api.JIPipeOrganization;
 import org.hkijena.jipipe.api.JIPipeProgressInfo;
+import org.hkijena.jipipe.api.data.JIPipeAnnotation;
 import org.hkijena.jipipe.api.data.JIPipeAnnotationMergeStrategy;
 import org.hkijena.jipipe.api.data.JIPipeData;
+import org.hkijena.jipipe.api.nodes.JIPipeDataBatch;
 import org.hkijena.jipipe.api.nodes.JIPipeInputSlot;
 import org.hkijena.jipipe.api.nodes.JIPipeMergingAlgorithm;
 import org.hkijena.jipipe.api.nodes.JIPipeMergingDataBatch;
 import org.hkijena.jipipe.api.nodes.JIPipeNodeInfo;
 import org.hkijena.jipipe.api.nodes.JIPipeOutputSlot;
+import org.hkijena.jipipe.api.nodes.JIPipeParameterSlotAlgorithm;
+import org.hkijena.jipipe.api.nodes.JIPipeSimpleIteratingAlgorithm;
 import org.hkijena.jipipe.api.nodes.categories.MiscellaneousNodeTypeCategory;
 import org.hkijena.jipipe.api.parameters.JIPipeParameter;
 import org.hkijena.jipipe.extensions.parameters.generators.IntegerRange;
+import org.hkijena.jipipe.utils.UIUtils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
-@JIPipeDocumentation(name = "Data batch slicer", description = "Merges the incoming data into merging data batches. Only outputs " +
-        "the items according to the index range. You can use this to remove duplicates. Annotations are not modified (merged annotations are not copied into " +
-        "the output).")
+@JIPipeDocumentation(name = "Limit data", description = "Slices the incoming data table into a smaller table with a limited row set according to the selected row.")
 @JIPipeInputSlot(value = JIPipeData.class, slotName = "Input", autoCreate = true)
 @JIPipeOutputSlot(value = JIPipeData.class, slotName = "Output", inheritedSlot = "Input", autoCreate = true)
 @JIPipeOrganization(nodeTypeCategory = MiscellaneousNodeTypeCategory.class)
-public class DataBatchSlicer extends JIPipeMergingAlgorithm {
+public class DataSlicer extends JIPipeParameterSlotAlgorithm {
 
-    private IntegerRange sliceRange = new IntegerRange("0");
+    private IntegerRange sliceRange = new IntegerRange("0-999");
 
-    public DataBatchSlicer(JIPipeNodeInfo info) {
+    public DataSlicer(JIPipeNodeInfo info) {
         super(info);
     }
 
-    public DataBatchSlicer(DataBatchSlicer other) {
+    public DataSlicer(DataSlicer other) {
         super(other);
         this.sliceRange = new IntegerRange(other.sliceRange);
     }
 
     @Override
-    protected void runIteration(JIPipeMergingDataBatch dataBatch, JIPipeProgressInfo progressInfo) {
+    public void runParameterSet(JIPipeProgressInfo progressInfo, List<JIPipeAnnotation> parameterAnnotations) {
         HashSet<Integer> indices = new HashSet<>(sliceRange.getIntegers());
-        ArrayList<Integer> rows = new ArrayList<>(dataBatch.getInputRows(getFirstInputSlot()));
-        for (int i = 0; i < rows.size(); i++) {
+        for (int i = 0; i < getFirstInputSlot().getRowCount(); i++) {
             if (indices.contains(i)) {
                 getFirstOutputSlot().addData(getFirstInputSlot().getVirtualData(i),
                         getFirstInputSlot().getAnnotations(i),
@@ -49,8 +52,8 @@ public class DataBatchSlicer extends JIPipeMergingAlgorithm {
         }
     }
 
-    @JIPipeDocumentation(name = "Slice range", description = "Only the items within this range are copied into the output. The first index is zero. " +
-            "If a data batch does not contain the requested index, no data is copied.\n\n" + IntegerRange.DOCUMENTATION_DESCRIPTION)
+    @JIPipeDocumentation(name = "Slice range", description = "Only the data rows within this range are copied into the output. The first index is zero. " +
+            "Invalid indices are ignored.\n\n" + IntegerRange.DOCUMENTATION_DESCRIPTION)
     @JIPipeParameter("slice-range")
     public IntegerRange getSliceRange() {
         return sliceRange;
