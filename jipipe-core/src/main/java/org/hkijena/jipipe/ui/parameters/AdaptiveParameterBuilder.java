51a99f10376457dba04479fb8f5f9cd0e05dbbc9
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2021-11-09 18:36:34 +0100

    * Continued adaptive parameter assistant

--- jipipe-core/src/main/java/org/hkijena/jipipe/ui/parameters/AdaptiveParameterBuilder.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/ui/parameters/AdaptiveParameterBuilder.java
@@ -1,15 +1,11 @@
 package org.hkijena.jipipe.ui.parameters;
 
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
 import org.hkijena.jipipe.JIPipe;
 import org.hkijena.jipipe.api.nodes.JIPipeGraphNode;
-import org.hkijena.jipipe.api.parameters.JIPipeManualParameterAccess;
 import org.hkijena.jipipe.api.parameters.JIPipeParameterAccess;
 import org.hkijena.jipipe.api.parameters.JIPipeParameterCollection;
 import org.hkijena.jipipe.api.parameters.JIPipeParameterTree;
 import org.hkijena.jipipe.api.parameters.JIPipeParameterTypeInfo;
-import org.hkijena.jipipe.extensions.expressions.DefaultExpressionParameter;
 import org.hkijena.jipipe.extensions.expressions.StringQueryExpression;
 import org.hkijena.jipipe.ui.JIPipeWorkbench;
 import org.hkijena.jipipe.ui.components.DocumentTabPane;
@@ -17,9 +13,7 @@ import org.hkijena.jipipe.ui.components.FormPanel;
 import org.hkijena.jipipe.ui.components.MarkdownDocument;
 import org.hkijena.jipipe.ui.components.ParameterTreeUI;
 import org.hkijena.jipipe.ui.components.ReadonlyCopyableTextField;
-import org.hkijena.jipipe.utils.StringUtils;
 import org.hkijena.jipipe.utils.UIUtils;
-import org.hkijena.jipipe.utils.json.JsonUtils;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -28,25 +22,26 @@ import javax.swing.tree.TreePath;
 import java.awt.BorderLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AdaptiveParameterBuilder extends JDialog {
     private final JIPipeWorkbench workbench;
     private final JIPipeParameterCollection parameterCollection;
     private final JIPipeParameterTree parameterTree;
     private ParameterTreeUI parameterTreeUI;
-    private JPanel contentPanel = new JPanel(new BorderLayout());
-    private FormPanel formPanel;
+    private final JPanel contentPanel = new JPanel(new BorderLayout());
+    private FormPanel listPanel;
+    private FormPanel infoPanel;
     private ReadonlyCopyableTextField nameLabel;
     private ReadonlyCopyableTextField typeLabel;
     private ReadonlyCopyableTextField nameIdLabel;
     private ReadonlyCopyableTextField typeIdLabel;
     private JTextPane typeDescriptionLabel;
-    private JPanel currentValuePanel = new JPanel(new BorderLayout());
-    private JPanel testerValuePanel = new JPanel(new BorderLayout());
     private boolean canceled = true;
+    private List<ConditionValuePair> conditionValuePairs = new ArrayList<>();
+    private JIPipeParameterAccess currentParameterAccess;
 
     public AdaptiveParameterBuilder(JIPipeWorkbench workbench, JIPipeParameterCollection parameterCollection) {
         this.workbench = workbench;
@@ -86,33 +81,50 @@ public class AdaptiveParameterBuilder extends JDialog {
         });
         getContentPane().add(splitPane, BorderLayout.CENTER);
 
-        formPanel = new FormPanel(null, FormPanel.WITH_SCROLLING | FormPanel.WITH_DOCUMENTATION | FormPanel.DOCUMENTATION_BELOW);
+        infoPanel = new FormPanel(null, FormPanel.WITH_SCROLLING | FormPanel.WITH_DOCUMENTATION | FormPanel.DOCUMENTATION_BELOW);
+        listPanel = new FormPanel(MarkdownDocument.fromPluginResource("documentation/adaptive-parameter-builder.md", Collections.emptyMap()),
+                FormPanel.WITH_SCROLLING | FormPanel.WITH_DOCUMENTATION | FormPanel.DOCUMENTATION_BELOW);
+
+        JPanel listContainer = new JPanel(new BorderLayout());
+        listContainer.add(listPanel, BorderLayout.CENTER);
+
+        JToolBar listToolbar = new JToolBar();
+        listToolbar.setFloatable(false);
+        listToolbar.add(Box.createHorizontalGlue());
+
+        JButton addConditionButton = new JButton("Add condition", UIUtils.getIconFromResources("actions/list-add.png"));
+        addConditionButton.addActionListener(e -> addNewConditionValuePair());
+        listToolbar.add(addConditionButton);
+
+        listContainer.add(listToolbar, BorderLayout.NORTH);
+
         DocumentTabPane documentTabPane = new DocumentTabPane();
-        documentTabPane.addTab("Info", UIUtils.getIconFromResources("actions/help-info.png"), formPanel, DocumentTabPane.CloseMode.withoutCloseButton);
+        documentTabPane.addTab("Conditions", UIUtils.getIconFromResources("actions/dialog-xml-editor.png"), listContainer, DocumentTabPane.CloseMode.withoutCloseButton);
+        documentTabPane.addTab("Info", UIUtils.getIconFromResources("actions/help-info.png"), infoPanel, DocumentTabPane.CloseMode.withoutCloseButton);
         contentPanel.add(documentTabPane, BorderLayout.CENTER);
 
-        initializeFormPanel();
+        initializeInfoPanel();
         initializeButtonPanel();
 
         // Register event
         parameterTreeUI.getTreeComponent().addTreeSelectionListener(e -> updateCurrentValue());
     }
 
-    private void initializeFormPanel() {
+    private void initializeInfoPanel() {
         nameLabel = new ReadonlyCopyableTextField("", false);
         nameIdLabel = new ReadonlyCopyableTextField("", true);
         typeLabel = new ReadonlyCopyableTextField("", false);
         typeIdLabel = new ReadonlyCopyableTextField("", true);
         typeDescriptionLabel = UIUtils.makeReadonlyTextPane("");
 
-        formPanel.addToForm(nameLabel, new JLabel("Name"), new MarkdownDocument("The name of the parameter"));
-        formPanel.addToForm(nameIdLabel, new JLabel("Unique ID"), new MarkdownDocument("The unique ID of the parameter. This is used for " +
+        infoPanel.addToForm(nameLabel, new JLabel("Name"), new MarkdownDocument("The name of the parameter"));
+        infoPanel.addToForm(nameIdLabel, new JLabel("Unique ID"), new MarkdownDocument("The unique ID of the parameter. This is used for " +
                 "creating new parameter data."));
-        formPanel.addToForm(typeLabel, new JLabel("Type name"), new MarkdownDocument("The name of the parameter type"));
-        formPanel.addToForm(typeIdLabel, new JLabel("Unique type ID"), new MarkdownDocument("The unique ID of the data type the parameter is " +
+        infoPanel.addToForm(typeLabel, new JLabel("Type name"), new MarkdownDocument("The name of the parameter type"));
+        infoPanel.addToForm(typeIdLabel, new JLabel("Unique type ID"), new MarkdownDocument("The unique ID of the data type the parameter is " +
                 "based on. On creating parameters, you will have to ensure that this type matches exactly."));
-        formPanel.addToForm(typeDescriptionLabel, new JLabel("Type description"), new MarkdownDocument("A short description of the parameter data type."));
-        formPanel.addVerticalGlue();
+        infoPanel.addToForm(typeDescriptionLabel, new JLabel("Type description"), new MarkdownDocument("A short description of the parameter data type."));
+        infoPanel.addVerticalGlue();
     }
 
     private void initializeButtonPanel() {
@@ -128,7 +140,7 @@ public class AdaptiveParameterBuilder extends JDialog {
         });
         buttonPanel.add(cancelButton);
 
-        JButton confirmButton = new JButton("Add", UIUtils.getIconFromResources("actions/list-add.png"));
+        JButton confirmButton = new JButton("Add adaptive parameter", UIUtils.getIconFromResources("actions/list-add.png"));
         confirmButton.addActionListener(e -> {
             canceled = false;
             setVisible(false);
@@ -140,41 +152,91 @@ public class AdaptiveParameterBuilder extends JDialog {
 
     private void updateCurrentValue() {
 
-        currentValuePanel.removeAll();
-        testerValuePanel.removeAll();
-
         nameLabel.setText("<Please select one parameter>");
         nameIdLabel.setText("");
         typeLabel.setText("");
         typeIdLabel.setText("");
         typeDescriptionLabel.setText("");
+        conditionValuePairs.clear();
+        currentParameterAccess = null;
+        rebuildConditionValuePairList();
 
         TreePath selectionPath = parameterTreeUI.getTreeComponent().getSelectionPath();
         if (selectionPath == null) {
-            formPanel.revalidate();
-            formPanel.repaint();
+            infoPanel.revalidate();
+            infoPanel.repaint();
             return;
         }
         DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selectionPath.getLastPathComponent();
         if (!(selectedNode.getUserObject() instanceof JIPipeParameterAccess)) {
-            formPanel.revalidate();
-            formPanel.repaint();
+            infoPanel.revalidate();
+            infoPanel.repaint();
             return;
         }
 
         JIPipeParameterAccess parameterAccess = (JIPipeParameterAccess) selectedNode.getUserObject();
         JIPipeParameterTypeInfo typeInfo = JIPipe.getParameterTypes().getInfoByFieldClass(parameterAccess.getFieldClass());
 
-        formPanel.getParameterHelp().setDocument(ParameterPanel.generateParameterDocumentation(parameterAccess, parameterTree));
+        infoPanel.getParameterHelp().setDocument(ParameterPanel.generateParameterDocumentation(parameterAccess, parameterTree));
 
         // Load info
+        currentParameterAccess = parameterAccess;
         nameLabel.setText(parameterAccess.getName());
         nameIdLabel.setText(parameterTree.getUniqueKey(parameterAccess));
         typeLabel.setText(typeInfo.getName());
         typeIdLabel.setText(typeInfo.getId());
         typeDescriptionLabel.setText(typeInfo.getDescription());
 
-        formPanel.revalidate();
-        formPanel.repaint();
+        infoPanel.revalidate();
+        infoPanel.repaint();
+
+        addNewConditionValuePair();
+    }
+
+    private void addNewConditionValuePair() {
+        if(currentParameterAccess != null) {
+            ConditionValuePair pair = new ConditionValuePair();
+            pair.setValueType(currentParameterAccess.getFieldClass());
+            pair.setValue(JIPipe.getParameterTypes().getInfoByFieldClass(currentParameterAccess.getFieldClass()).newInstance());
+            conditionValuePairs.add(pair);
+        }
+    }
+
+    private void rebuildConditionValuePairList() {
+
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public static class ConditionValuePair {
+        private StringQueryExpression condition = new StringQueryExpression();
+        private Object value;
+        private Class<?> valueType;
+
+        public StringQueryExpression getCondition() {
+            return condition;
+        }
+
+        public void setCondition(StringQueryExpression condition) {
+            this.condition = condition;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public Class<?> getValueType() {
+            return valueType;
+        }
+
+        public void setValueType(Class<?> valueType) {
+            this.valueType = valueType;
+        }
     }
 }
---
+++ jipipe-core/src/main/resources/org/hkijena/jipipe/documentation/adaptive-parameter-builder.md
@@ -0,0 +1,6 @@
+# Build adaptive parameters
+
+First, select the parameter that should be made adaptive. 
+Add new conditions into the list by clicking the *Add* button at the top right.
+JIPipe will check each condition expression in order. If it matches, then 
+the value assigned to the condition will be set as parameter.
