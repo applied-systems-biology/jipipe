88edc6bc6cddf418f8367bfe804051afdfcbfe57
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-02-18 00:42:10 +0100

    + Started implementing IJ2

--- jipipe-ij2/src/main/java/org/hkijena/jipipe/extensions/imagej2/ImageJ2NodeInfo.java
+++ jipipe-ij2/src/main/java/org/hkijena/jipipe/extensions/imagej2/ImageJ2NodeInfo.java
@@ -3,6 +3,8 @@ package org.hkijena.jipipe.extensions.imagej2;
 import org.hkijena.jipipe.JIPipeDependency;
 import org.hkijena.jipipe.api.nodes.*;
 import org.hkijena.jipipe.extensions.parameters.library.markup.HTMLText;
+import org.scijava.command.Command;
+import org.scijava.plugin.PluginInfo;
 
 import java.util.List;
 import java.util.Set;
@@ -11,29 +13,38 @@ import java.util.Set;
  * A {@link JIPipeNodeInfo} implementation that reads its information from an ImageJ2 {@link org.scijava.command.Command}
  */
 public class ImageJ2NodeInfo implements JIPipeNodeInfo {
+
+    private final PluginInfo<Command> pluginInfo;
+    private final String id;
+
+    public ImageJ2NodeInfo(PluginInfo<Command> pluginInfo) {
+        this.pluginInfo = pluginInfo;
+        this.id = "ij2:" + pluginInfo.getIdentifier();
+    }
+
     @Override
     public String getId() {
-        return null;
+        return id;
     }
 
     @Override
     public Class<? extends JIPipeGraphNode> getInstanceClass() {
-        return null;
+        return ImageJ2Algorithm.class;
     }
 
     @Override
     public JIPipeGraphNode newInstance() {
-        return null;
+        return new ImageJ2Algorithm(this);
     }
 
     @Override
     public JIPipeGraphNode duplicate(JIPipeGraphNode algorithm) {
-        return null;
+        return new ImageJ2Algorithm((ImageJ2Algorithm) algorithm);
     }
 
     @Override
     public String getName() {
-        return null;
+        return "Imagej2 " + pluginInfo.getName();
     }
 
     @Override
@@ -69,5 +80,9 @@ public class ImageJ2NodeInfo implements JIPipeNodeInfo {
     @Override
     public boolean isHidden() {
         return false;
+    }
+
+    public PluginInfo<Command> getPluginInfo() {
+        return pluginInfo;
     }
 }
