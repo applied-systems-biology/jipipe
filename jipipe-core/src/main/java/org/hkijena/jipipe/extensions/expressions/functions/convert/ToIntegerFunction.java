31415cadb4f148ea46178feab48150797934291a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-03-13 17:12:27 +0100

    + To Integer function

---
+++ jipipe-core/src/main/java/org/hkijena/jipipe/extensions/expressions/functions/convert/ToIntegerFunction.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright by Zoltán Cseresnyés, Ruman Gerst
+ *
+ * Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo Figge
+ * https://www.leibniz-hki.de/en/applied-systems-biology.html
+ * HKI-Center for Systems Biology of Infection
+ * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Institute (HKI)
+ * Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+ *
+ * The project code is licensed under BSD 2-Clause.
+ * See the LICENSE file provided with the code for the full license.
+ */
+
+package org.hkijena.jipipe.extensions.expressions.functions.convert;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.hkijena.jipipe.api.JIPipeDocumentation;
+import org.hkijena.jipipe.extensions.expressions.ExpressionFunction;
+import org.hkijena.jipipe.extensions.expressions.ExpressionVariables;
+import org.hkijena.jipipe.extensions.expressions.ParameterInfo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@JIPipeDocumentation(name = "Convert to integer", description = "Converts the input to an integral number. Strings must be formatted correctly. Boolean values are converted to TRUE = 1 and FALSE = 0")
+public class ToIntegerFunction extends ExpressionFunction {
+
+    public ToIntegerFunction() {
+        super("TO_NUMBER", 1);
+    }
+
+    @Override
+    public ParameterInfo getParameterInfo(int index) {
+        if (index == 0) {
+            return new ParameterInfo("value", "The value to convert");
+        }
+        return null;
+    }
+
+    @Override
+    public Object evaluate(List<Object> parameters, ExpressionVariables variables) {
+        Object value = parameters.get(0);
+        if (value instanceof Collection) {
+            List<Object> result = new ArrayList<>();
+            for (Object item : ((Collection<?>) value)) {
+                result.add(convert(item));
+            }
+            return result;
+        } else {
+            return convert(value);
+        }
+    }
+
+    public Object convert(Object value) {
+        if (value instanceof Number)
+            return ((Number) value).intValue();
+        else if (value instanceof Boolean)
+            return (boolean) value ? 1 : 0;
+        else
+            return NumberUtils.createInteger("" + value);
+    }
+}
