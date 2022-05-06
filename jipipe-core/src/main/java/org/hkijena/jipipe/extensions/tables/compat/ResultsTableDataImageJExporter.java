c68f28519c9f898db564fc7e042146e8e3f67739
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-05-06 13:45:11 +0200

    + Importing results tables

--- jipipe-core/src/main/java/org/hkijena/jipipe/extensions/tables/compat/ResultsTableDataImageJExporter.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/extensions/tables/compat/ResultsTableDataImageJExporter.java
@@ -8,17 +8,19 @@ import org.hkijena.jipipe.api.compat.ImageJExportParameters;
 import org.hkijena.jipipe.api.data.JIPipeData;
 import org.hkijena.jipipe.api.data.JIPipeDataTable;
 import org.hkijena.jipipe.extensions.tables.datatypes.ResultsTableData;
+import org.hkijena.jipipe.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@JIPipeDocumentation(name = "Export to results table", description = "Exports tables into the 'Results' table")
+@JIPipeDocumentation(name = "Export to results table", description = "Exports tables into an ImageJ table. Leave the name empty or set it to 'Results' to export it to the standard 'Results' table.")
 public class ResultsTableDataImageJExporter implements ImageJDataExporter {
     @Override
     public List<Object> exportData(JIPipeDataTable dataTable, ImageJExportParameters parameters, JIPipeProgressInfo progressInfo) {
         List<Object> result = new ArrayList<>();
         if (parameters.isActivate()) {
-            ResultsTable resultsTable = ResultsTable.getResultsTable();
+            String tableName = StringUtils.orElse(parameters.getName(), "Results");
+            ResultsTable resultsTable = "Results".equals(tableName) ? ResultsTable.getResultsTable() : new ResultsTable();
             if (!parameters.isAppend()) {
                 resultsTable.reset();
             }
@@ -28,7 +30,7 @@ public class ResultsTableDataImageJExporter implements ImageJDataExporter {
                 data.addToTable(resultsTable);
             }
             if (!parameters.isNoWindows()) {
-                ResultsTable.getResultsTable().show("Results");
+                resultsTable.show(tableName);
             }
         } else {
             for (int i = 0; i < dataTable.getRowCount(); i++) {
--- jipipe-core/src/main/java/org/hkijena/jipipe/extensions/tables/compat/ResultsTableDataImageJImporter.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/extensions/tables/compat/ResultsTableDataImageJImporter.java
@@ -1,6 +1,8 @@
 package org.hkijena.jipipe.extensions.tables.compat;
 
+import ij.WindowManager;
 import ij.measure.ResultsTable;
+import ij.text.TextWindow;
 import org.hkijena.jipipe.api.JIPipeDocumentation;
 import org.hkijena.jipipe.api.JIPipeProgressInfo;
 import org.hkijena.jipipe.api.compat.ImageJDataImporter;
@@ -8,10 +10,12 @@ import org.hkijena.jipipe.api.compat.ImageJImportParameters;
 import org.hkijena.jipipe.api.data.JIPipeData;
 import org.hkijena.jipipe.api.data.JIPipeDataTable;
 import org.hkijena.jipipe.extensions.tables.datatypes.ResultsTableData;
+import org.hkijena.jipipe.utils.StringUtils;
 
+import java.awt.Frame;
 import java.util.List;
 
-@JIPipeDocumentation(name = "Import results table", description = "Imports a results table")
+@JIPipeDocumentation(name = "Import results table", description = "Imports a results table. To import a table other than the default 'Results' table, customize the name.")
 public class ResultsTableDataImageJImporter implements ImageJDataImporter {
     @Override
     public JIPipeDataTable importData(List<Object> objects, ImageJImportParameters parameters, JIPipeProgressInfo progressInfo) {
@@ -21,7 +25,18 @@ public class ResultsTableDataImageJImporter implements ImageJDataImporter {
                 dataTable.addData(new ResultsTableData((ResultsTable) object), new JIPipeProgressInfo());
             }
         } else {
-            dataTable.addData(new ResultsTableData((ResultsTable) ResultsTable.getResultsTable().clone()), new JIPipeProgressInfo());
+            String tableName = StringUtils.orElse(parameters.getName(), "Results");
+            ResultsTable resultsTable = null;
+            if("Results".equals(tableName)) {
+                resultsTable = ResultsTable.getResultsTable();
+            }
+            else {
+                Frame frame = WindowManager.getFrame(tableName);
+                if(frame instanceof TextWindow) {
+                    resultsTable = ((TextWindow) frame).getResultsTable();
+                }
+            }
+            dataTable.addData(new ResultsTableData((ResultsTable) resultsTable.clone()), new JIPipeProgressInfo());
         }
         return dataTable;
     }
