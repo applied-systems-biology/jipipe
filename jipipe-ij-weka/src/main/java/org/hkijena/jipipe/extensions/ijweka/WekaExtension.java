11e645f51bfb8316ee580fdcacf07010dc1852d1
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2022-05-17 00:01:57 +0200

    * work on Weka

--- jipipe-ij-weka/src/main/java/org/hkijena/jipipe/extensions/ijweka/WekaExtension.java
+++ jipipe-ij-weka/src/main/java/org/hkijena/jipipe/extensions/ijweka/WekaExtension.java
@@ -19,6 +19,7 @@ import org.hkijena.jipipe.api.JIPipeProgressInfo;
 import org.hkijena.jipipe.extensions.JIPipePrepackagedDefaultJavaExtension;
 import org.hkijena.jipipe.extensions.ijweka.datatypes.WekaModelData;
 import org.hkijena.jipipe.extensions.ijweka.nodes.ImportWekaModelFromFileAlgorithm;
+import org.hkijena.jipipe.extensions.ijweka.nodes.WekaClassificationAlgorithm;
 import org.hkijena.jipipe.extensions.ijweka.nodes.WekaTrainingROI2DAlgorithm;
 import org.hkijena.jipipe.extensions.parameters.library.markup.HTMLText;
 import org.hkijena.jipipe.extensions.parameters.library.primitives.list.StringList;
@@ -66,6 +67,7 @@ public class WekaExtension extends JIPipePrepackagedDefaultJavaExtension {
         registerDatatype("weka-model", WekaModelData.class, wekaModelIcon);
         registerNodeType("import-weka-model-from-file", ImportWekaModelFromFileAlgorithm.class, wekaIcon);
         registerNodeType("weka-training-roi-2d", WekaTrainingROI2DAlgorithm.class, wekaIcon);
+        registerNodeType("weka-classification", WekaClassificationAlgorithm.class, wekaIcon);
     }
 
     @Override
---
+++ jipipe-ij-weka/src/main/java/org/hkijena/jipipe/extensions/ijweka/nodes/WekaClassificationAlgorithm.java
@@ -0,0 +1,35 @@
+package org.hkijena.jipipe.extensions.ijweka.nodes;
+
+import ij.ImagePlus;
+import org.hkijena.jipipe.api.JIPipeDocumentation;
+import org.hkijena.jipipe.api.JIPipeNode;
+import org.hkijena.jipipe.api.JIPipeProgressInfo;
+import org.hkijena.jipipe.api.nodes.*;
+import org.hkijena.jipipe.api.nodes.categories.ImagesNodeTypeCategory;
+import org.hkijena.jipipe.extensions.ijweka.datatypes.WekaModelData;
+import org.hkijena.jipipe.extensions.imagejdatatypes.datatypes.ImagePlusData;
+import trainableSegmentation.WekaSegmentation;
+
+@JIPipeDocumentation(name = "Weka classifier", description = "Classifies an image with a Weka model")
+@JIPipeNode(nodeTypeCategory = ImagesNodeTypeCategory.class, menuPath = "Weka")
+@JIPipeInputSlot(value = ImagePlusData.class, slotName = "Image", description = "Image on which the classification should be applied", autoCreate = true)
+@JIPipeInputSlot(value = WekaModelData.class, slotName = "Model", description = "The model", autoCreate = true)
+@JIPipeOutputSlot(value = ImagePlusData.class, slotName = "Classified image", description = "The classified image", autoCreate = true)
+public class WekaClassificationAlgorithm extends JIPipeIteratingAlgorithm {
+    public WekaClassificationAlgorithm(JIPipeNodeInfo info) {
+        super(info);
+    }
+
+    public WekaClassificationAlgorithm(WekaClassificationAlgorithm other) {
+        super(other);
+    }
+
+    @Override
+    protected void runIteration(JIPipeDataBatch dataBatch, JIPipeProgressInfo progressInfo) {
+        ImagePlusData image = dataBatch.getInputData("Image", ImagePlusData.class, progressInfo);
+        WekaModelData modelData = dataBatch.getInputData("Model", WekaModelData.class, progressInfo);
+        WekaSegmentation segmentation = modelData.getSegmentation();
+        ImagePlus classified = segmentation.applyClassifier(image.getImage());
+        dataBatch.addOutputData("Classified image", new ImagePlusData(classified), progressInfo);
+    }
+}
