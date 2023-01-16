a922dd47b486f6333086a24367bc7c3882d6b6f2
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-01-16 15:00:56 +0100

    + Crop ROI list + MTM: add export of templates

--- jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
+++ jipipe-ij-algorithms/src/main/java/org/hkijena/jipipe/extensions/imagejalgorithms/ImageJAlgorithmsExtension.java
@@ -1139,6 +1139,7 @@ public class ImageJAlgorithmsExtension extends JIPipePrepackagedDefaultJavaExten
         registerNodeType("ij1-roi-to-mask-unreferenced", UnreferencedRoiToMaskAlgorithm.class, UIUtils.getIconURLFromResources("actions/segment.png"));
         registerNodeType("ij1-roi-to-mask", RoiToMaskAlgorithm.class, UIUtils.getIconURLFromResources("actions/segment.png"));
         registerNodeType("ij1-roi-outline", OutlineRoiAlgorithm.class, UIUtils.getIconURLFromResources("actions/draw-connector.png"));
+        registerNodeType("ij1-roi-crop-list", CropRoiListAlgorithm.class, UIUtils.getIconURLFromResources("actions/image-crop.png"));
         registerNodeType("ij1-roi-to-centroid", RoiToCentroidAlgorithm.class, UIUtils.getIconURLFromResources("actions/draw-connector.png"));
         registerNodeType("ij1-roi-remove-bordering", RemoveBorderRoisAlgorithm.class, UIUtils.getIconURLFromResources("actions/bordertool.png"));
         registerNodeType("ij1-roi-statistics", RoiStatisticsAlgorithm.class, UIUtils.getIconURLFromResources("actions/statistics.png"));
