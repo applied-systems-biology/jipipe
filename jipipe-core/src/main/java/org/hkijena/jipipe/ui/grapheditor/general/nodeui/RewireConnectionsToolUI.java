5f7edf7a193894d56d1f9aed36b0b79a6f9f330c
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-02-02 23:24:14 +0100

    * work

--- jipipe-core/src/main/java/org/hkijena/jipipe/ui/grapheditor/general/nodeui/RewireConnectionsToolUI.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/ui/grapheditor/general/nodeui/RewireConnectionsToolUI.java
@@ -1,6 +1,8 @@
 package org.hkijena.jipipe.ui.grapheditor.general.nodeui;
 
 import org.hkijena.jipipe.api.data.JIPipeDataSlot;
+import org.hkijena.jipipe.ui.components.FormPanel;
+import org.hkijena.jipipe.utils.AutoResizeSplitPane;
 import org.hkijena.jipipe.utils.UIUtils;
 
 import javax.swing.*;
@@ -24,8 +26,21 @@ public class RewireConnectionsToolUI extends JDialog {
         setContentPane(new JPanel(new BorderLayout()));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
+        AutoResizeSplitPane splitPane = new AutoResizeSplitPane(AutoResizeSplitPane.LEFT_RIGHT, AutoResizeSplitPane.RATIO_1_TO_1);
+        FormPanel connectionsList = new FormPanel(FormPanel.WITH_SCROLLING);
+        JList<JIPipeDataSlot> alternativesList = new JList<>();
+
+        splitPane.setLeftComponent(connectionsList);
+        splitPane.setRightComponent(alternativesList);
+
+        initializeConnectionsList(connectionsList);
+
         pack();
         setModal(true);
         setSize(800,600);
+    }
+
+    private void initializeConnectionsList(FormPanel connectionsList) {
+        connectionsList.addGroupHeader("List of rewired connections", UIUtils.getIconFromResources(""))
     }
 }
