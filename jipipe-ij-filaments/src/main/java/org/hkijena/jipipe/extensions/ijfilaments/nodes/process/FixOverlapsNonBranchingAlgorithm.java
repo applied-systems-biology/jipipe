dd32f03c2c5fc552f0acef81d5e91687de015fb8
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-02-11 01:21:17 +0100

    * work

--- jipipe-ij-filaments/src/main/java/org/hkijena/jipipe/extensions/ijfilaments/nodes/process/FixOverlapsNonBranchingAlgorithm.java
+++ jipipe-ij-filaments/src/main/java/org/hkijena/jipipe/extensions/ijfilaments/nodes/process/FixOverlapsNonBranchingAlgorithm.java
@@ -1,21 +1,15 @@
 package org.hkijena.jipipe.extensions.ijfilaments.nodes.process;
 
-import com.google.common.collect.ImmutableList;
-import gnu.trove.map.TObjectDoubleMap;
-import gnu.trove.map.hash.TObjectDoubleHashMap;
 import org.hkijena.jipipe.api.JIPipeDocumentation;
 import org.hkijena.jipipe.api.JIPipeNode;
 import org.hkijena.jipipe.api.JIPipeProgressInfo;
 import org.hkijena.jipipe.api.nodes.*;
-import org.hkijena.jipipe.api.parameters.JIPipeParameter;
-import org.hkijena.jipipe.extensions.expressions.DefaultExpressionParameter;
-import org.hkijena.jipipe.extensions.expressions.ExpressionParameterSettings;
-import org.hkijena.jipipe.extensions.expressions.ExpressionParameterSettingsVariable;
 import org.hkijena.jipipe.extensions.ijfilaments.FilamentsNodeTypeCategory;
 import org.hkijena.jipipe.extensions.ijfilaments.datatypes.FilamentsData;
 import org.hkijena.jipipe.extensions.ijfilaments.util.FilamentEdge;
 import org.hkijena.jipipe.extensions.ijfilaments.util.FilamentVertex;
-import org.hkijena.jipipe.utils.ColorUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jgrapht.Graphs;
 import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.scijava.vecmath.Vector3d;
 
@@ -44,64 +38,101 @@ public class FixOverlapsNonBranchingAlgorithm extends JIPipeSimpleIteratingAlgor
         FilamentsData filamentsData = new FilamentsData(dataBatch.getInputData(getFirstInputSlot(), FilamentsData.class, progressInfo));
         Map<FilamentVertex, Integer> components = filamentsData.findComponentIds();
 
-        TODO: First collect all candidates and then apply
-
         // Remove all junction nodes (degree > 2)
         filamentsData.removeVertexIf(vertex -> filamentsData.degreeOf(vertex) > 2);
 
         // Detect candidates to be connected (degree == 1)
-        Set<FilamentVertex> toConnect = filamentsData.vertexSet().stream().filter(vertex -> filamentsData.degreeOf(vertex) == 1).collect(Collectors.toSet());
+        Set<FilamentVertex> unconnected = filamentsData.vertexSet().stream().filter(vertex -> filamentsData.degreeOf(vertex) == 1).collect(Collectors.toSet());
+
 
         // For each candidate: search for best matching vertex within radius that is within the same original component
         // The other candidate should also not be connected to the current candidate
-        // Score: abs(scalar product)
-        Set<FilamentVertex> candidates = new HashSet<>();
-        Map<FilamentVertex, Double> candidateScores = new HashMap<>();
-        int successes = 0;
+        // Score: abs(scalar product of normalized vectors)
+        Set<EdgeCandidate> candidates = new TreeSet<>();
         ConnectivityInspector<FilamentVertex, FilamentEdge> inspector = new ConnectivityInspector<>(filamentsData);
         Random random = new Random();
-        while(!toConnect.isEmpty()) {
-            candidates.clear();
-            candidateScores.clear();
+        for (FilamentVertex current : unconnected) {
 
-            FilamentVertex current = toConnect.iterator().next();
-            toConnect.remove(current);
-            Vector3d currentCentroid = current.getCentroid().toNormalizedVector3d();
+            Vector3d currentV1 = current.getCentroid().toVector3d();
+            Vector3d currentV2 = Graphs.neighborSetOf(filamentsData, current).iterator().next().getCentroid().toVector3d();
+            Vector3d currentDirection = new Vector3d(currentV1.x - currentV2.x, currentV1.y - currentV2.y, currentV1.z - currentV2.z);
+            currentDirection.normalize();
 
-            for (FilamentVertex other : toConnect) {
-                // TODO: visibility (Z/C/T)
-                if(other != current && Objects.equals(components.get(other), components.get(current)) && other.getCentroid().distanceTo(current.getCentroid()) < 50 && !inspector.pathExists(current, other)) { // TODO: configurable formula
+            for (FilamentVertex other : unconnected) {
+                if(other != current &&
+                        Objects.equals(components.get(current), components.get(other)) &&
+                        other.getCentroid().distanceTo(current.getCentroid()) < 50  &&
+                        !inspector.pathExists(current, other)) {
+                    // TODO: visibility (Z/C/T)
                     // TODO: allow to restrict angle
-                    candidates.add(other);
+
+                    // TODO: Prefer shorter connections
+                    // TODO: maybe do a raytrace to check for collisions?
+
+                    // Calculate score
+                    Vector3d otherV1 = other.getCentroid().toVector3d();
+                    Vector3d otherV2 = Graphs.neighborSetOf(filamentsData, other).iterator().next().getCentroid().toVector3d();
+                    Vector3d otherDirection = new Vector3d(otherV1.x - otherV2.x, otherV1.y - otherV2.y, otherV1.z - otherV2.z);
+                    otherDirection.normalize();
+
+                    double score = Math.abs( currentDirection.dot(otherDirection) );
+
+                    // Add as candidate
+                    EdgeCandidate candidate = new EdgeCandidate(current, other, score);
+                    candidates.add(candidate);
+
                 }
             }
+        }
 
-            // Calculate score
-            for (FilamentVertex candidate : candidates) {
-                Vector3d candidateCentroid = candidate.getCentroid().toNormalizedVector3d(); // TODO: custom formula
-                double score = Math.abs( currentCentroid.dot(candidateCentroid) );
-                candidateScores.put(candidate, score);
-            }
-
-            // Choose best score
-            if(!candidateScores.isEmpty()) {
-                FilamentVertex bestCandidate = candidateScores.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
-
+        // Apply connect
+        int successes = 0;
+        Set<FilamentVertex> processedCandidateVertices = new HashSet<>();
+        for (EdgeCandidate candidate : candidates) {
+            if(!processedCandidateVertices.contains(candidate.source) && !processedCandidateVertices.contains(candidate.target)) {
                 // Connect
-                FilamentEdge edge = filamentsData.addEdge(current, bestCandidate);
+                FilamentEdge edge = filamentsData.addEdge(candidate.source, candidate.target);
                 edge.setColor(Color.getHSBColor(random.nextFloat(), 1, 1));
-                toConnect.remove(bestCandidate);
-                inspector = new ConnectivityInspector<>(filamentsData);
+
+                // Mark as used
+                processedCandidateVertices.add(candidate.source);
+                processedCandidateVertices.add(candidate.target);
                 ++successes;
-            }
-            else {
-                progressInfo.log("Unable to find candidate for " + current.getUuid());
             }
         }
 
         progressInfo.log("Successfully created " + successes + " edges.");
 
         dataBatch.addOutputData(getFirstOutputSlot(), filamentsData, progressInfo);
+    }
+
+    public static class EdgeCandidate implements Comparable<EdgeCandidate> {
+        private final FilamentVertex source;
+        private final FilamentVertex target;
+        private final double score;
+
+        public EdgeCandidate(FilamentVertex source, FilamentVertex target, double score) {
+            this.source = source;
+            this.target = target;
+            this.score = score;
+        }
+
+        public FilamentVertex getSource() {
+            return source;
+        }
+
+        public FilamentVertex getTarget() {
+            return target;
+        }
+
+        public double getScore() {
+            return score;
+        }
+
+        @Override
+        public int compareTo(@NotNull FixOverlapsNonBranchingAlgorithm.EdgeCandidate o) {
+            return -Double.compare(score, o.score);
+        }
     }
 
 }
