c8202a0213048f516c92f8c5a339efd2f3753b13
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-08-10 16:06:22 +0200

    + GET_PARENT_DIRECTORY now can have Nth parent

--- jipipe-core/src/main/java/org/hkijena/jipipe/extensions/expressions/functions/filesystem/GetParentDirectoryFunction.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/extensions/expressions/functions/filesystem/GetParentDirectoryFunction.java
@@ -6,29 +6,34 @@ import org.hkijena.jipipe.extensions.expressions.ExpressionVariables;
 import org.hkijena.jipipe.extensions.expressions.ParameterInfo;
 import org.hkijena.jipipe.utils.StringUtils;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-@JIPipeDocumentation(name = "Get parent directory", description = "Returns the parent directory of the provided file/folder path.")
+@JIPipeDocumentation(name = "Get parent directory", description = "Returns the Nth (default 1) parent directory of the provided file/folder path.")
 public class GetParentDirectoryFunction extends ExpressionFunction {
     public GetParentDirectoryFunction() {
-        super("GET_PARENT_DIRECTORY", 1);
+        super("GET_PARENT_DIRECTORY", 1, 2);
     }
 
     @Override
     public Object evaluate(List<Object> parameters, ExpressionVariables variables) {
         String pathString = StringUtils.nullToEmpty(parameters.get(0));
-        try {
-            return Paths.get(pathString).getParent().toString();
-        } catch (Exception e) {
-            return false;
+        int N = parameters.size() > 1 ? (int)StringUtils.parseDouble(StringUtils.orElse(parameters.get(1), "1")) : 1;
+        Path path = Paths.get(pathString);
+        for (int i = 0; i < N; i++) {
+            path = path.getParent();
         }
+        return path.toString();
     }
 
     @Override
     public ParameterInfo getParameterInfo(int index) {
         if (index == 0) {
             return new ParameterInfo("Path", "String that contains the path", String.class);
+        }
+        else if(index == 1) {
+            return new ParameterInfo("N", "Selects the Nth parent", Number.class);
         }
         return null;
     }
