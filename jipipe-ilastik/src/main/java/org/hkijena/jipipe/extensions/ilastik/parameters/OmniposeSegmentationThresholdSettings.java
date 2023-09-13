7aedc02867bc77ae00ca0e25c2751af38d26094f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-09-13 18:20:42 +0200

    + Added project for ilastik * pushed version to 1.82.0

---
+++ jipipe-ilastik/src/main/java/org/hkijena/jipipe/extensions/ilastik/parameters/OmniposeSegmentationThresholdSettings.java
@@ -0,0 +1,81 @@
+package org.hkijena.jipipe.extensions.omnipose.parameters;
+
+import org.hkijena.jipipe.api.JIPipeDocumentation;
+import org.hkijena.jipipe.api.parameters.AbstractJIPipeParameterCollection;
+import org.hkijena.jipipe.api.parameters.JIPipeParameter;
+import org.hkijena.jipipe.extensions.parameters.library.primitives.optional.OptionalDoubleParameter;
+
+public class OmniposeSegmentationThresholdSettings extends AbstractJIPipeParameterCollection {
+    private double flowThreshold = 0.4;
+    private double maskThreshold = 0;
+
+    private double diamThreshold = 12;
+    private OptionalDoubleParameter stitchThreshold = new OptionalDoubleParameter(0, false);
+    private boolean excludeOnEdges = false;
+
+    public OmniposeSegmentationThresholdSettings() {
+    }
+
+    public OmniposeSegmentationThresholdSettings(OmniposeSegmentationThresholdSettings other) {
+        this.flowThreshold = other.flowThreshold;
+        this.maskThreshold = other.maskThreshold;
+        this.stitchThreshold = other.stitchThreshold;
+        this.excludeOnEdges = other.excludeOnEdges;
+        this.diamThreshold = other.diamThreshold;
+    }
+
+    @JIPipeDocumentation(name = "Exclude masks on edges", description = "Discard masks which touch edges of image")
+    @JIPipeParameter("exclude-on-edges")
+    public boolean isExcludeOnEdges() {
+        return excludeOnEdges;
+    }
+
+    @JIPipeParameter("exclude-on-edges")
+    public void setExcludeOnEdges(boolean excludeOnEdges) {
+        this.excludeOnEdges = excludeOnEdges;
+    }
+
+    @JIPipeDocumentation(name = "Flow threshold (2D)", description = "Flow error threshold, 0 turns off this optional QC step. Default: 0.4")
+    @JIPipeParameter("flow-threshold")
+    public double getFlowThreshold() {
+        return flowThreshold;
+    }
+
+    @JIPipeParameter("flow-threshold")
+    public void setFlowThreshold(double flowThreshold) {
+        this.flowThreshold = flowThreshold;
+    }
+
+    @JIPipeDocumentation(name = "Mask threshold", description = "Mask threshold, default is 0, decrease to find more and larger masks")
+    @JIPipeParameter("mask-threshold")
+    public double getMaskThreshold() {
+        return maskThreshold;
+    }
+
+    @JIPipeParameter("mask-threshold")
+    public void setMaskThreshold(double maskThreshold) {
+        this.maskThreshold = maskThreshold;
+    }
+
+    @JIPipeDocumentation(name = "Cell diameter threshold", description = "Cell diameter threshold for upscaling before mask reconstruction, default 12.")
+    @JIPipeParameter("diam-threshold")
+    public double getDiamThreshold() {
+        return diamThreshold;
+    }
+
+    @JIPipeParameter("diam-threshold")
+    public void setDiamThreshold(double diamThreshold) {
+        this.diamThreshold = diamThreshold;
+    }
+
+    @JIPipeDocumentation(name = "Stitch threshold", description = "Compute masks in 2D then stitch together masks with IoU>0.9 across planes")
+    @JIPipeParameter("stitch-threshold")
+    public OptionalDoubleParameter getStitchThreshold() {
+        return stitchThreshold;
+    }
+
+    @JIPipeParameter("stitch-threshold")
+    public void setStitchThreshold(OptionalDoubleParameter stitchThreshold) {
+        this.stitchThreshold = stitchThreshold;
+    }
+}
---
+++ jipipe-ilastik/src/main/java/org/hkijena/jipipe/extensions/ilastik/parameters/OmniposeSegmentationTweaksSettings.java
@@ -0,0 +1,96 @@
+package org.hkijena.jipipe.extensions.omnipose.parameters;
+
+import org.hkijena.jipipe.api.JIPipeDocumentation;
+import org.hkijena.jipipe.api.parameters.AbstractJIPipeParameterCollection;
+import org.hkijena.jipipe.api.parameters.JIPipeParameter;
+import org.hkijena.jipipe.extensions.parameters.library.primitives.optional.OptionalDoubleParameter;
+
+public class OmniposeSegmentationTweaksSettings extends AbstractJIPipeParameterCollection {
+    private boolean netAverage = true;
+    private boolean interpolate = true;
+    private OptionalDoubleParameter anisotropy = new OptionalDoubleParameter(1.0, false);
+    private boolean disableResample = false;
+
+    private boolean fastMode = false;
+    private boolean cluster = false;
+
+
+    public OmniposeSegmentationTweaksSettings() {
+    }
+
+    public OmniposeSegmentationTweaksSettings(OmniposeSegmentationTweaksSettings other) {
+        this.netAverage = other.netAverage;
+        this.interpolate = other.interpolate;
+        this.anisotropy = new OptionalDoubleParameter(other.anisotropy);
+        this.disableResample = other.disableResample;
+        this.cluster = other.cluster;
+        this.fastMode = other.fastMode;
+    }
+
+    @JIPipeDocumentation(name = "Fast mode", description = "disable dynamics on full image (makes algorithm faster for images with large diameters)")
+    @JIPipeParameter("fast-mode")
+    public boolean isFastMode() {
+        return fastMode;
+    }
+
+    @JIPipeParameter("fast-mode")
+    public void setFastMode(boolean fastMode) {
+        this.fastMode = fastMode;
+    }
+
+    @JIPipeDocumentation(name = "Cluster", description = "DBSCAN clustering. Reduces oversegmentation of thin features")
+    @JIPipeParameter("cluster")
+    public boolean isCluster() {
+        return cluster;
+    }
+
+    @JIPipeParameter("cluster")
+    public void setCluster(boolean cluster) {
+        this.cluster = cluster;
+    }
+
+    @JIPipeDocumentation(name = "Anisotropy (3D)", description = "For 3D segmentation, optional rescaling factor " +
+            "(e.g. set to 2.0 if Z is sampled half as dense as X or Y)")
+    @JIPipeParameter("anisotropy")
+    public OptionalDoubleParameter getAnisotropy() {
+        return anisotropy;
+    }
+
+    @JIPipeParameter("anisotropy")
+    public void setAnisotropy(OptionalDoubleParameter anisotropy) {
+        this.anisotropy = anisotropy;
+    }
+
+    @JIPipeDocumentation(name = "Average all networks", description = "Runs the 4 built-in networks and averages them if True, runs one network if disabled")
+    @JIPipeParameter("net-average")
+    public boolean isNetAverage() {
+        return netAverage;
+    }
+
+    @JIPipeParameter("net-average")
+    public void setNetAverage(boolean netAverage) {
+        this.netAverage = netAverage;
+    }
+
+    @JIPipeDocumentation(name = "Interpolate (2D)", description = "Interpolate during 2D dynamics (not available in 3D)")
+    @JIPipeParameter("interpolate")
+    public boolean isInterpolate() {
+        return interpolate;
+    }
+
+    @JIPipeParameter("interpolate")
+    public void setInterpolate(boolean interpolate) {
+        this.interpolate = interpolate;
+    }
+
+    @JIPipeDocumentation(name = "Disable resample", description = "Disable dynamics on full image (makes algorithm faster for images with large diameters)")
+    @JIPipeParameter("no-resample")
+    public boolean isDisableResample() {
+        return disableResample;
+    }
+
+    @JIPipeParameter("no-resample")
+    public void setDisableResample(boolean disableResample) {
+        this.disableResample = disableResample;
+    }
+}
