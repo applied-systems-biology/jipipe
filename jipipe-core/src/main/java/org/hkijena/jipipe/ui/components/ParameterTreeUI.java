805c1e88d672b6693f56de894aef0c4002baf91f
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2024-03-14 00:31:15 +0100

    * fix

--- jipipe-core/src/main/java/org/hkijena/jipipe/ui/components/ParameterTreeUI.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/ui/components/ParameterTreeUI.java
@@ -82,11 +82,11 @@ public class ParameterTreeUI extends JPanel {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 
-        JButton collapseAllButton = new JButton("Collapse all", UIUtils.getIconFromResources("actions/tree.png"));
+        JButton collapseAllButton = new JButton("Collapse all", UIUtils.getIconFromResources("actions/collapse-all.png"));
         collapseAllButton.addActionListener(e -> UIUtils.setTreeExpandedState(ui.treeComponent, false));
         buttonPanel.add(collapseAllButton);
 
-        JButton expandAllButton = new JButton("Expand all", UIUtils.getIconFromResources("actions/tree.png"));
+        JButton expandAllButton = new JButton("Expand all", UIUtils.getIconFromResources("actions/expand-all.png"));
         expandAllButton.addActionListener(e -> UIUtils.setTreeExpandedState(ui.treeComponent, true));
         buttonPanel.add(expandAllButton);
 
