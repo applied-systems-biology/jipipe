5c60c013c57da18a35e09219d8ed2f20eabe062a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-01-16 10:26:54 +0100

    + ability to upgrade string and numeric parameters to expressions

--- jipipe-core/src/main/java/org/hkijena/jipipe/plugins/expressions/JIPipeExpressionParameter.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/plugins/expressions/JIPipeExpressionParameter.java
@@ -13,16 +13,26 @@
 
 package org.hkijena.jipipe.plugins.expressions;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.hkijena.jipipe.JIPipe;
 import org.hkijena.jipipe.plugins.parameters.api.collections.ListParameter;
 import org.hkijena.jipipe.utils.scripting.MacroUtils;
 
+import javax.crypto.Mac;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 
 /**
  * An {@link AbstractExpressionParameter} that utilizes the {@link JIPipeExpressionEvaluator} to generate results
  */
+@JsonSerialize(using = JIPipeExpressionParameter.Serializer.class)
+@JsonDeserialize(using = JIPipeExpressionParameter.Deserializer.class)
 public class JIPipeExpressionParameter extends AbstractExpressionParameter {
     private static JIPipeExpressionEvaluator EVALUATOR;
 
@@ -52,6 +62,35 @@ public class JIPipeExpressionParameter extends AbstractExpressionParameter {
         return EVALUATOR;
     }
 
+    /**
+     * Escapes a string
+     * @param s the string
+     * @param addQuotes if quotes should be added
+     * @return the escaped string
+     */
+    public static String escapeString(String s, boolean addQuotes) {
+        if(addQuotes) {
+            return String.format("\"%s\"", MacroUtils.escapeString(s));
+        }
+        else {
+            return MacroUtils.escapeString(s);
+        }
+    }
+
+    /**
+     * Escapes a string as valid expression (with quotes)
+     * @param s the string
+     * @return the escaped string
+     */
+    public static String escapeString(String s) {
+        return escapeString(s, true);
+    }
+
+    /**
+     * Returns true if a string is a valid variable name
+     * @param key the string
+     * @return if the key is a valid variable name
+     */
     public static boolean isValidVariableName(String key) {
         return MacroUtils.isValidVariableName(key);
     }
@@ -79,6 +118,39 @@ public class JIPipeExpressionParameter extends AbstractExpressionParameter {
             for (JIPipeExpressionParameter parameter : other) {
                 add(new JIPipeExpressionParameter(parameter));
             }
+        }
+    }
+
+    public static class Deserializer extends JsonDeserializer<JIPipeExpressionParameter> {
+        @Override
+        public JIPipeExpressionParameter deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            JsonNode root = p.readValueAsTree();
+            if(root.isNumber()) {
+                // Upgrade a numeric parameter to expression
+                return new JIPipeExpressionParameter(root.numberValue().toString());
+            }
+            else if(root.isTextual()) {
+                return new JIPipeExpressionParameter(JIPipeExpressionParameter.escapeString(root.textValue(), true));
+            }
+            else if(root.isObject()) {
+                String expression = "";
+                if(root.has("expression")) {
+                    expression = root.get("expression").asText();
+                }
+                return new JIPipeExpressionParameter(expression);
+            }
+            else {
+                return new JIPipeExpressionParameter();
+            }
+        }
+    }
+
+    public static class Serializer extends JsonSerializer<JIPipeExpressionParameter> {
+        @Override
+        public void serialize(JIPipeExpressionParameter value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeStartObject();
+            gen.writeStringField("expression", value.getExpression());
+            gen.writeEndObject();
         }
     }
 }
