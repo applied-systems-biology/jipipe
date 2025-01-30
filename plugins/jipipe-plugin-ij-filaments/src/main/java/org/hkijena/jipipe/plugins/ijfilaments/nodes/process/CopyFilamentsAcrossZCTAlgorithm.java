45b87a5aa98627d742a03c41524289e8502a5cd4
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-01-30 18:26:28 +0100

    * work

--- plugins/jipipe-plugin-ij-filaments/src/main/java/org/hkijena/jipipe/plugins/ijfilaments/nodes/process/CopyFilamentsAcrossZCTAlgorithm.java
+++ plugins/jipipe-plugin-ij-filaments/src/main/java/org/hkijena/jipipe/plugins/ijfilaments/nodes/process/CopyFilamentsAcrossZCTAlgorithm.java
@@ -23,10 +23,15 @@ import org.hkijena.jipipe.api.nodes.JIPipeNodeInfo;
 import org.hkijena.jipipe.api.nodes.algorithm.JIPipeIteratingAlgorithm;
 import org.hkijena.jipipe.api.nodes.iterationstep.JIPipeIterationContext;
 import org.hkijena.jipipe.api.nodes.iterationstep.JIPipeSingleIterationStep;
+import org.hkijena.jipipe.api.parameters.JIPipeParameter;
+import org.hkijena.jipipe.plugins.expressions.AddJIPipeExpressionParameterVariable;
 import org.hkijena.jipipe.plugins.expressions.JIPipeExpressionParameter;
 import org.hkijena.jipipe.plugins.ijfilaments.FilamentsNodeTypeCategory;
 import org.hkijena.jipipe.plugins.ijfilaments.datatypes.Filaments3DGraphData;
+import org.hkijena.jipipe.plugins.ijfilaments.parameters.VertexMaskParameter;
 import org.hkijena.jipipe.plugins.imagejdatatypes.datatypes.ImagePlusData;
+import org.hkijena.jipipe.plugins.imagejdatatypes.util.HyperstackDimension;
+import org.hkijena.jipipe.plugins.imagejdatatypes.util.ImagePlusPropertiesExpressionParameterVariablesInfo;
 
 @SetJIPipeDocumentation(name = "Copy filaments across Z/C/T", description = "Copies a filament graph to other Z locations, channels, and frames. The newly created vertices can be optionally connected to the neighboring source vertex.")
 @ConfigureJIPipeNode(nodeTypeCategory = FilamentsNodeTypeCategory.class, menuPath = "Process")
@@ -35,20 +40,57 @@ import org.hkijena.jipipe.plugins.imagejdatatypes.datatypes.ImagePlusData;
 @AddJIPipeOutputSlot(value = Filaments3DGraphData.class, name = "Output", create = true)
 public class CopyFilamentsAcrossZCTAlgorithm extends JIPipeIteratingAlgorithm {
 
-    private JIPipeExpressionParameter channelRange = new JIPipeExpressionParameter("channelRange");
-    private JIPipeExpressionParameter frameRange = new JIPipeExpressionParameter("frameRange");
-    private JIPipeExpressionParameter zRange = new JIPipeExpressionParameter("zRange");
+    private HyperstackDimension dimension = HyperstackDimension.Frame;
+    private JIPipeExpressionParameter locations = new JIPipeExpressionParameter("RANGE(0, num_t)");
+    private final VertexMaskParameter vertexMask;
 
     public CopyFilamentsAcrossZCTAlgorithm(JIPipeNodeInfo info) {
         super(info);
+        this.vertexMask = new VertexMaskParameter();
+        registerSubParameter(vertexMask);
     }
 
     public CopyFilamentsAcrossZCTAlgorithm(CopyFilamentsAcrossZCTAlgorithm other) {
         super(other);
+        this.dimension = other.dimension;
+        this.locations = new JIPipeExpressionParameter(other.locations);
+        this.vertexMask = new VertexMaskParameter(other.vertexMask);
+        registerSubParameter(vertexMask);
     }
 
     @Override
     protected void runIteration(JIPipeSingleIterationStep iterationStep, JIPipeIterationContext iterationContext, JIPipeGraphNodeRunContext runContext, JIPipeProgressInfo progressInfo) {
+        Filaments3DGraphData filaments = (Filaments3DGraphData) iterationStep.getInputData("Input", Filaments3DGraphData.class, progressInfo).duplicate(progressInfo);
+        ImagePlusData referenceImageData = iterationStep.getInputData("Reference", ImagePlusData.class, progressInfo);
 
+    }
+
+    @SetJIPipeDocumentation(name = "Direction", description = "The direction in which to expand each vertex.")
+    @JIPipeParameter("dimension")
+    public HyperstackDimension getDimension() {
+        return dimension;
+    }
+
+    @JIPipeParameter("dimension")
+    public void setDimension(HyperstackDimension dimension) {
+        this.dimension = dimension;
+    }
+
+    @SetJIPipeDocumentation(name = "Locations (in direction)", description = "Expression that determines the locations in the selected direction where the vertex will be present.")
+    @JIPipeParameter("locations")
+    @AddJIPipeExpressionParameterVariable(fromClass = ImagePlusPropertiesExpressionParameterVariablesInfo.class)
+    public JIPipeExpressionParameter getLocations() {
+        return locations;
+    }
+
+    @JIPipeParameter("locations")
+    public void setLocations(JIPipeExpressionParameter locations) {
+        this.locations = locations;
+    }
+
+    @SetJIPipeDocumentation(name = "Vertex mask", description = "Used to filter vertices")
+    @JIPipeParameter("vertex-filter")
+    public VertexMaskParameter getVertexMask() {
+        return vertexMask;
     }
 }
