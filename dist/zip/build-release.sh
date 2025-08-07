6a7d41d516563b80a3145468cc824e528cf68d43
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-08-07 17:26:55 +0200

    * updated dist scripts

--- dist/zip/build-release.sh
+++ dist/zip/build-release.sh
@@ -198,7 +198,7 @@ for component in jipipe-core jipipe-desktop jipipe-cli jipipe-plugin-clij jipipe
 done
 
 cp -rv ./dependencies/* ./package/jars/
-for component in cwljava-1.2.0; do
+for component in jipipe-cwljava-1.2.0 jipipe-ro-crate-java-2.1.0; do
 	cp -v ../../contrib/$component/target/$component-SNAPSHOT.jar ./package/jars/$component.jar
 	cp -v ../../contrib/$component/target/$component.jar ./package/jars/$component.jar
 done
