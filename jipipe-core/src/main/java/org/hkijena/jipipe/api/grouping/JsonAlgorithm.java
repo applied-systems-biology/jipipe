d440559f555bd92ff94b3ec7147435a0b2f3d06a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-09-04 17:36:29 +0200

    * started removal of JIPipeGraphViewMode.java

--- jipipe-core/src/main/java/org/hkijena/jipipe/api/grouping/JsonAlgorithm.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/api/grouping/JsonAlgorithm.java
@@ -64,7 +64,7 @@ public class JsonAlgorithm extends JIPipeGraphWrapperAlgorithm implements JIPipe
         group.setEnabled(algorithm.isEnabled());
         group.setPassThrough(algorithm.isPassThrough());
         group.setWrappedGraph(new JIPipeGraph(algorithm.getWrappedGraph()));
-        group.setNodeUILocationPerViewModePerCompartment(algorithm.getNodeUILocationPerViewModePerCompartment());
+        group.getMetadata().putAll(algorithm.getMetadata());
 
         List<Map.Entry<JIPipeDataSlot, JIPipeDataSlot>> edges = new ArrayList<>();
         for (Map.Entry<JIPipeDataSlot, JIPipeDataSlot> edge : graph.getSlotEdges()) {
