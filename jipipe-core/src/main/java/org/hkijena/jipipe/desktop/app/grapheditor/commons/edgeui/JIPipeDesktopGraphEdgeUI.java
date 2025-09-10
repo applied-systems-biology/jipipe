bcfca7368231fc96a7187b54342c3a4041a90055
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-09-10 16:05:58 +0200

    * split functionality

--- jipipe-core/src/main/java/org/hkijena/jipipe/desktop/app/grapheditor/commons/edgeui/JIPipeDesktopGraphEdgeUI.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/desktop/app/grapheditor/commons/edgeui/JIPipeDesktopGraphEdgeUI.java
@@ -144,33 +144,45 @@ public class JIPipeDesktopGraphEdgeUI implements JIPipeDesktopGraphInteractiveOb
             JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode arrowHeadMode = enableArrows ? JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.Filled : JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.None;
 
             if(multiColor) {
-                g.setStroke(strokeBorder);
-                Color edgeColor = canvasUI.getResources().getEdgeColor(source, target, multiColor, multiColorIndex, multiColorMax);
-                Color edgeOutlineColor = ThemeUtils.isUsingDarkTheme() ? edgeColor.brighter() : edgeColor.darker();
-
-                g.setColor(edgeOutlineColor);
-                canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
-                g.setStroke(stroke);
-
-                g.setPaint(edgeColor);
-                canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+                paintMultiColor(g, stroke, strokeBorder, scale, viewX, viewY, multiColor, multiColorIndex, multiColorMax, sourcePoint, sourceNodeUI, targetPoint, uiShape, arrowHeadMode);
             }
             else {
-                g.setStroke(strokeBorder);
-                g.setColor(canvasUI.getResources().getEdgeColor(source, target, false, 0, 0));
-                canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
-                g.setStroke(stroke);
-
-                g.setPaint(canvasUI.getResources().getEdgeBackgroundPaint(source, target, sourcePoint, targetPoint, canvasUI.getResources().getImprovedStrokeBackgroundColor()));
-                canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+                paintRegular(g, stroke, strokeBorder, scale, viewX, viewY, sourcePoint, sourceNodeUI, targetPoint, uiShape, arrowHeadMode);
             }
         } else {
             // Just a single stroke
-            JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode arrowHeadMode = enableArrows ? JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.Thin : JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.None;
-
-            g.setStroke(stroke);
-            g.setColor(canvasUI.getResources().getEdgeColor(source, target, multiColor, multiColorIndex, multiColorMax));
-            canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+            paintThin(g, stroke, scale, viewX, viewY, enableArrows, multiColor, multiColorIndex, multiColorMax, sourcePoint, sourceNodeUI, targetPoint, uiShape);
         }
+    }
+
+    private void paintThin(Graphics2D g, Stroke stroke, double scale, int viewX, int viewY, boolean enableArrows, boolean multiColor, int multiColorIndex, int multiColorMax, PointRange sourcePoint, JIPipeDesktopGraphNodeUI sourceNodeUI, PointRange targetPoint, JIPipeGraphEdge.Shape uiShape) {
+        JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode arrowHeadMode = enableArrows ? JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.Thin : JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode.None;
+
+        g.setStroke(stroke);
+        g.setColor(canvasUI.getResources().getEdgeColor(source, target, multiColor, multiColorIndex, multiColorMax));
+        canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+    }
+
+    private void paintRegular(Graphics2D g, Stroke stroke, Stroke strokeBorder, double scale, int viewX, int viewY, PointRange sourcePoint, JIPipeDesktopGraphNodeUI sourceNodeUI, PointRange targetPoint, JIPipeGraphEdge.Shape uiShape, JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode arrowHeadMode) {
+        g.setStroke(strokeBorder);
+        g.setColor(canvasUI.getResources().getEdgeColor(source, target, false, 0, 0));
+        canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+        g.setStroke(stroke);
+
+        g.setPaint(canvasUI.getResources().getEdgeBackgroundPaint(source, target, sourcePoint, targetPoint, canvasUI.getResources().getImprovedStrokeBackgroundColor()));
+        canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+    }
+
+    private void paintMultiColor(Graphics2D g, Stroke stroke, Stroke strokeBorder, double scale, int viewX, int viewY, boolean multiColor, int multiColorIndex, int multiColorMax, PointRange sourcePoint, JIPipeDesktopGraphNodeUI sourceNodeUI, PointRange targetPoint, JIPipeGraphEdge.Shape uiShape, JIPipeDesktopGraphCanvasPaintManager.ArrowHeadMode arrowHeadMode) {
+        g.setStroke(strokeBorder);
+        Color edgeColor = canvasUI.getResources().getEdgeColor(source, target, multiColor, multiColorIndex, multiColorMax);
+        Color edgeOutlineColor = ThemeUtils.isUsingDarkTheme() ? edgeColor.brighter() : edgeColor.darker();
+
+        g.setColor(edgeOutlineColor);
+        canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
+        g.setStroke(stroke);
+
+        g.setPaint(edgeColor);
+        canvasUI.getPaintManager().paintEdge(g, sourcePoint.center, sourceNodeUI.getBounds(), targetPoint.center, uiShape, scale, viewX, viewY, arrowHeadMode);
     }
 }
