64da6a2f0f84c661d67acf134081ef68c5d4942a
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-10-02 00:52:42 +0200

    * fix #1136

--- jipipe-core/src/main/java/org/hkijena/jipipe/utils/ArchiveUtils.java
+++ jipipe-core/src/main/java/org/hkijena/jipipe/utils/ArchiveUtils.java
@@ -136,58 +136,8 @@ public class ArchiveUtils {
         return destFile;
     }
 
-    public static void decompressTarGZ(Path zipFile, Path targetDir, JIPipeProgressInfo progressInfo) throws IOException {
-        try (FileInputStream in = new FileInputStream(zipFile.toFile())) {
-            GzipCompressorInputStream gzipIn = new GzipCompressorInputStream(in);
-            try (TarArchiveInputStream tarIn = new TarArchiveInputStream(gzipIn)) {
-                TarArchiveEntry entry;
-
-                Map<Path, Path> createdLinks = new HashMap<>();
-
-                while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
-                    String entryName = StringUtils.nullTerminate(entry.getName());
-
-                    Path entryOutputPath = targetDir.resolve(Paths.get(entryName));
-                    progressInfo.log("Entry " + entryName + " -> " + entryOutputPath);
-
-                    if (entry.isSymbolicLink() || entry.isLink()) {
-                        Path linkName = Paths.get(StringUtils.nullTerminate(entry.getLinkName()));
-                        if (!linkName.isAbsolute()) {
-                            linkName = entryOutputPath.getParent().resolve(linkName);
-                        }
-                        // Needs to be deferred
-                        createdLinks.put(linkName, entryOutputPath);
-                    } else if (entry.isDirectory()) {
-                        Files.createDirectories(entryOutputPath);
-                    } else if (entry.isFile()) {
-
-                        // Ensure that parent directories exist
-                        Files.createDirectories(entryOutputPath.getParent());
-
-                        int count;
-                        byte[] data = new byte[BUFFER_SIZE];
-                        try (FileOutputStream fos = new FileOutputStream(entryOutputPath.toFile(), false)) {
-                            try (BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER_SIZE)) {
-                                while ((count = tarIn.read(data, 0, BUFFER_SIZE)) != -1) {
-                                    dest.write(data, 0, count);
-                                }
-                            }
-                        }
-                    } else {
-                        progressInfo.log("Unsupported entry: " + entryName);
-                    }
-                }
-
-                for (Map.Entry<Path, Path> pathEntry : createdLinks.entrySet()) {
-                    Path linkName = pathEntry.getKey();
-                    Path entryOutputPath = pathEntry.getValue();
-                    progressInfo.log("Linking " + entryOutputPath + " -> " + linkName);
-                    Files.createDirectories(entryOutputPath.getParent());
-                    Files.createSymbolicLink(entryOutputPath, linkName);
-                }
-
-            }
-        }
+    public static void decompressTarGZ(Path tarGzFile, Path targetDir, JIPipeProgressInfo progressInfo) throws IOException {
+       TarGzExtractor.decompressTarGZ(tarGzFile, targetDir, progressInfo);
     }
 
     /**
