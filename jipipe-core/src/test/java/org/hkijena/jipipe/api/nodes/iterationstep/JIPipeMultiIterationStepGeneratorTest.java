cff46e40aed051815775297ed7deb57970bf0688
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2025-10-11 02:00:19 +0200

    * refined tests

--- jipipe-core/src/test/java/org/hkijena/jipipe/api/nodes/iterationstep/JIPipeMultiIterationStepGeneratorTest.java
+++ jipipe-core/src/test/java/org/hkijena/jipipe/api/nodes/iterationstep/JIPipeMultiIterationStepGeneratorTest.java
@@ -41,6 +41,31 @@ class JIPipeMultiIterationStepGeneratorTest {
     }
 
     /**
+     * A basic test that applies a simple iteration case with non-reference columns
+     * Because only # columns are used as reference, all other columns are ignored during creation of iteration steps
+     */
+    @Test
+    public void testSimpleIterationWithNonReference() {
+        JIPipeMultiIterationStepGenerator generator = new JIPipeMultiIterationStepGenerator();
+        generator.setSlots(List.of(createDummyTable("Input",
+                column("#Dataset", "A", "B", "C"),
+                column("Ignored", "1", "2", "3"))));
+        generator.setReferenceColumns(JIPipeIterationStepTextAnnotationColumMatching.PrefixHashUnion, new StringQueryExpression());
+        generator.setApplyMerging(false);
+        List<JIPipeMultiIterationStep> result = generator.build(JIPipeProgressInfo.SILENT);
+
+        // Expect 3 iteration steps
+        assertEquals(3, result.size());
+
+        // Expect the following layout
+        assertEquals(expectedResult(
+                step(slot("Input", 0)),
+                step(slot("Input", 1)),
+                step(slot("Input", 2))
+        ), build(generator));
+    }
+
+    /**
      * A basic test that applies a simple merging case, but all reference column values are different
      * Uses the default selection method (use # to denote important columns)
      */
@@ -85,5 +110,45 @@ class JIPipeMultiIterationStepGeneratorTest {
         ), build(generator));
     }
 
+    /**
+     * Test the all into one batch setting
+     */
+    @Test
+    public void testAllIntoOne() {
+        JIPipeMultiIterationStepGenerator generator = new JIPipeMultiIterationStepGenerator();
+        generator.setSlots(List.of(createDummyTable("Input", column("#Dataset", "A", "B", "C"))));
+        generator.setReferenceColumns(JIPipeIterationStepTextAnnotationColumMatching.MergeAll, new StringQueryExpression());
+        generator.setApplyMerging(true);
+        List<JIPipeMultiIterationStep> result = generator.build(JIPipeProgressInfo.SILENT);
+
+        // Expect 1 step
+        assertEquals(1, result.size());
+
+        // Expect the following layout
+        assertEquals(expectedResult(
+                step(slot("Input", 0, 1, 2))
+        ), build(generator));
+    }
+
+    /**
+     * Test the all into one batch setting but with merging disabled
+     * (this will be ignored by the algorithm, which then cause expected issues with {@link JIPipeSingleIterationStep} creation.
+     */
+    @Test
+    public void testAllIntoOne2() {
+        JIPipeMultiIterationStepGenerator generator = new JIPipeMultiIterationStepGenerator();
+        generator.setSlots(List.of(createDummyTable("Input", column("#Dataset", "A", "B", "C"))));
+        generator.setReferenceColumns(JIPipeIterationStepTextAnnotationColumMatching.MergeAll, new StringQueryExpression());
+        generator.setApplyMerging(false);
+        List<JIPipeMultiIterationStep> result = generator.build(JIPipeProgressInfo.SILENT);
+
+        // Expect 1 step
+        assertEquals(1, result.size());
+
+        // Expect the following layout
+        assertEquals(expectedResult(
+                step(slot("Input", 0, 1, 2))
+        ), build(generator));
+    }
 
 }
